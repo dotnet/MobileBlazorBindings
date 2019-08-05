a1ba123c482676b0e8c7a68ccb33f551776a03ff
Eilon Lipton <Eilon@users.noreply.github.com>
2019-08-05 14:19:38 -0700

    Add Left/Width/Height properties

--- BlinForms.Framework/Controls/FormsComponentBase.cs
+++ BlinForms.Framework/Controls/FormsComponentBase.cs
@@ -8,12 +8,18 @@ namespace BlinForms.Framework.Controls
     public abstract class FormsComponentBase : ComponentBase
     {
         [Parameter] public int Top { get; set; }
+        [Parameter] public int Left { get; set; }
+        [Parameter] public int Width { get; set; }
+        [Parameter] public int Height { get; set; }
 
         protected override void BuildRenderTree(RenderTreeBuilder builder)
         {
             builder.OpenElement(0, GetType().FullName);
             RenderAttributes(builder);
             builder.AddAttribute(100, nameof(Top), Top);
+            builder.AddAttribute(200, nameof(Left), Left);
+            builder.AddAttribute(300, nameof(Width), Width);
+            builder.AddAttribute(400, nameof(Height), Height);
             builder.CloseElement();
         }
 
@@ -24,7 +30,20 @@ namespace BlinForms.Framework.Controls
             switch (attributeFrame.AttributeName)
             {
                 case nameof(Top):
-                    control.Top = int.Parse((string)attributeFrame.AttributeValue);
+                    if ((attributeFrame.AttributeValue as string) != "0")
+                        control.Top = int.Parse((string)attributeFrame.AttributeValue);
+                    break;
+                case nameof(Left):
+                    if ((attributeFrame.AttributeValue as string) != "0")
+                        control.Left = int.Parse((string)attributeFrame.AttributeValue);
+                    break;
+                case nameof(Width):
+                    if ((attributeFrame.AttributeValue as string) != "0")
+                        control.Width = int.Parse((string)attributeFrame.AttributeValue);
+                    break;
+                case nameof(Height):
+                    if ((attributeFrame.AttributeValue as string) != "0")
+                        control.Height = int.Parse((string)attributeFrame.AttributeValue);
                     break;
                 default:
                     throw new NotImplementedException($"FormsComponentBase doesn't recognize attribute '{attributeFrame.AttributeName}'");
