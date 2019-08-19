84bdc4f5ca11177aede288599203a4fc7f75886f
Eilon Lipton <Eilon@users.noreply.github.com>
2019-08-19 15:52:03 -0700

    Add empty Xamarin.Forms project
    With iOS and Android backends.

---
+++ BlaxamarinSample/BlaxamarinSample/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using BlaxamarinSample.Models;
+
+namespace BlaxamarinSample.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
