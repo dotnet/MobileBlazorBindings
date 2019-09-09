2da0337c77e5b2caa4e04b35a8a92a853b4fd389
Eilon Lipton <Eilon@users.noreply.github.com>
2019-09-09 09:19:34 -0700

    AppState initial code

---
+++ BlinFormsSample/AppState.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BlinFormsSample
+{
+    public class AppState
+    {
+        public List<TodoItem> items = new List<TodoItem>();
+
+        public int Counter { get; set; }
+
+        public void ResetAppState()
+        {
+            items.AddRange(
+                new[]
+                {
+                    new TodoItem { Text = "sell dog", IsDone = true },
+                    new TodoItem { Text = "buy cat" },
+                    new TodoItem { Text = "buy cat food" },
+                });
+
+            Counter = 0;
+        }
+
+        public bool IsEmptyAppState()
+        {
+            return
+                !items.Any() &&
+                Counter == 0;
+        }
+    }
+}
