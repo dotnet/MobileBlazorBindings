b2aa565d8e2cedb62e30054e7837529dde5564a4
Eilon Lipton <Eilon@users.noreply.github.com>
2019-10-03 16:57:45 -0700

    Remove YAML comments

--- azure-pipelines/artifacts/deployables.ps1
+++ azure-pipelines/artifacts/deployables.ps1
@@ -1,21 +1,13 @@
 $RepoRoot = [System.IO.Path]::GetFullPath("$PSScriptRoot\..\..")
 $BuildConfiguration = $env:BUILDCONFIGURATION
 
-Write-Host "BuildConfiguration(1) = $BuildConfiguration"
-
 if (!$BuildConfiguration) {
     $BuildConfiguration = 'Debug'
 }
 
-Write-Host "BuildConfiguration(2) = $BuildConfiguration"
-
 $PackagesRoot = "$RepoRoot/bin/Packages/$BuildConfiguration"
 
-Write-Host "PackagesRoot = $PackagesRoot"
-
 if (!(Test-Path $PackagesRoot))  { return }
-
-Write-Host "Found folder, looking for files..."
 
 @{
     "$PackagesRoot" = (Get-ChildItem $PackagesRoot -Recurse)
