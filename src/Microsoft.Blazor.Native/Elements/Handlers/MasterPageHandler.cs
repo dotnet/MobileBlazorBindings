9a1bb821581fe4e6e4bd47de94d002313941fe2d
Eilon Lipton <Eilon@users.noreply.github.com>
2019-10-22 13:15:58 -0700

    Minor code cleanup

--- src/Microsoft.Blazor.Native/Elements/Handlers/MasterPageHandler.cs
+++ src/Microsoft.Blazor.Native/Elements/Handlers/MasterPageHandler.cs
@@ -1,12 +1,14 @@
 ﻿using Emblazon;
+using System;
 using XF = Xamarin.Forms;
+
 namespace Microsoft.Blazor.Native.Elements.Handlers
 {
     public sealed class MasterPageHandler : ContentPageHandler
     {
         public MasterPageHandler(EmblazonRenderer renderer, XF.ContentPage masterDetailPageControl) : base(renderer, masterDetailPageControl)
         {
-            MasterDetailPageControl = masterDetailPageControl ?? throw new System.ArgumentNullException(nameof(masterDetailPageControl));
+            MasterDetailPageControl = masterDetailPageControl ?? throw new ArgumentNullException(nameof(masterDetailPageControl));
 
             // The Master page must have its Title set:
             // https://github.com/xamarin/Xamarin.Forms/blob/ff63ef551d9b2b5736092eb48aaf954f54d63417/Xamarin.Forms.Core/MasterDetailPage.cs#L72
--- src/Microsoft.Blazor.Native/Elements/Handlers/ModalContainerHandler.cs
+++ src/Microsoft.Blazor.Native/Elements/Handlers/ModalContainerHandler.cs
@@ -2,90 +2,87 @@
 using System;
 using XF = Xamarin.Forms;
 
-namespace Microsoft.Blazor.Native.Elements
+namespace Microsoft.Blazor.Native.Elements.Handlers
 {
-
-    namespace Handlers
+    public class ModalContainerHandler : IXamarinFormsElementHandler, INonChildContainerElement
     {
-        public class ModalContainerHandler : IXamarinFormsElementHandler, INonChildContainerElement
+        private XF.NavigableElement _modalParent;
+        private XF.Page _modalChild;
+
+        public ModalContainerHandler(EmblazonRenderer renderer, ModalContainerPlaceholderElement modalContainerPlaceholderElementControl)
         {
-            private XF.NavigableElement _modalParent;
-            private XF.Page _modalChild;
+            Renderer = renderer ?? throw new ArgumentNullException(nameof(renderer));
+            ModalContainerPlaceholderElementControl = modalContainerPlaceholderElementControl ?? throw new ArgumentNullException(nameof(modalContainerPlaceholderElementControl));
+        }
 
-            public ModalContainerHandler(EmblazonRenderer renderer, ModalContainerPlaceholderElement modalContainerPlaceholderElementControl)
+        public EmblazonRenderer Renderer { get; }
+        public ModalContainerPlaceholderElement ModalContainerPlaceholderElementControl { get; }
+        public XF.Element ElementControl => ModalContainerPlaceholderElementControl;
+        public object TargetElement => ElementControl;
+
+        public XF.NavigableElement ModalParent
+        {
+            get => _modalParent;
+            set
             {
-                Renderer = renderer ?? throw new ArgumentNullException(nameof(renderer));
-                ModalContainerPlaceholderElementControl = modalContainerPlaceholderElementControl ?? throw new ArgumentNullException(nameof(modalContainerPlaceholderElementControl));
+                _modalParent = value;
+                ShowDialogIfPossible();
             }
+        }
 
-            public EmblazonRenderer Renderer { get; }
-            public XF.Element ModalContainerPlaceholderElementControl { get; }
-            public XF.Element ElementControl => ModalContainerPlaceholderElementControl;
-            public object TargetElement => ElementControl;
-
-            public XF.NavigableElement ModalParent
+        public XF.Page ModalChild
+        {
+            get => _modalChild;
+            set
             {
-                get => _modalParent;
-                set
+                _modalChild = value;
+                if (_modalChild != null)
                 {
-                    _modalParent = value;
-                    ShowDialogIfPossible();
+                    _modalChild.Disappearing += CleanUpDisappearingPage;
                 }
+                ShowDialogIfPossible();
             }
-            public XF.Page ModalChild
+        }
+
+        private void CleanUpDisappearingPage(object sender, EventArgs e)
+        {
+            _modalChild.Disappearing -= CleanUpDisappearingPage;
+
+            ModalChild = null;
+            ModalParent = null;
+
+            if (ClosedEventHandlerId != default)
             {
-                get => _modalChild;
-                set
-                {
-                    _modalChild = value;
-                    if (_modalChild != null)
+                Renderer.Dispatcher.InvokeAsync(() => Renderer.DispatchEventAsync(ClosedEventHandlerId, null, e));
+            }
+        }
+
+        public ulong ClosedEventHandlerId { get; set; }
+
+        public void ApplyAttribute(ulong attributeEventHandlerId, string attributeName, object attributeValue, string attributeEventUpdatesAttributeName)
+        {
+            switch (attributeName)
+            {
+                case "__ShowDialog":
+                    if (attributeValue != null)
                     {
-                        _modalChild.Disappearing += CleanUpDisappearingPage;
+                        ShowDialogIfPossible();
                     }
-                    ShowDialogIfPossible();
-                }
+                    break;
+                case "onclosed":
+                    Renderer.RegisterEvent(attributeEventHandlerId, () => ClosedEventHandlerId = 0);
+                    ClosedEventHandlerId = attributeEventHandlerId;
+                    break;
+                default:
+                    throw new NotImplementedException($"{GetType().FullName} doesn't recognize attribute '{attributeName}'");
             }
+        }
 
-            private void CleanUpDisappearingPage(object sender, EventArgs e)
+        private void ShowDialogIfPossible()
+        {
+            if (ModalParent != null && ModalChild != null)
             {
-                _modalChild.Disappearing -= CleanUpDisappearingPage;
-
-                ModalChild = null;
-                ModalParent = null;
-
-                if (ClosedEventHandlerId != default)
-                {
-                    Renderer.Dispatcher.InvokeAsync(() => Renderer.DispatchEventAsync(ClosedEventHandlerId, null, e));
-                }
-            }
-
-            public ulong ClosedEventHandlerId { get; set; }
-
-            public void ApplyAttribute(ulong attributeEventHandlerId, string attributeName, object attributeValue, string attributeEventUpdatesAttributeName)
-            {
-                switch (attributeName)
-                {
-                    case "__ShowDialog":
-                        if (attributeValue != null)
-                        {
-                            ShowDialogIfPossible();
-                        }
-                        break;
-                    case "onclosed":
-                        Renderer.RegisterEvent(attributeEventHandlerId, () => ClosedEventHandlerId = 0);
-                        ClosedEventHandlerId = attributeEventHandlerId;
-                        break;
-                    default:
-                        throw new NotImplementedException($"{GetType().FullName} doesn't recognize attribute '{attributeName}'");
-                }
-            }
-
-            private void ShowDialogIfPossible()
-            {
-                if (ModalParent != null && ModalChild != null)
-                {
-                    ModalParent.Navigation.PushModalAsync(ModalChild);
-                }
+                ModalParent.Navigation.PushModalAsync(ModalChild);
             }
         }
     }
