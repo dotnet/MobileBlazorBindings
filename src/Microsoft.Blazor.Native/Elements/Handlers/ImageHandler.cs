37c581f93621cfdffe91acd3bd840300ee996fee
Eilon Lipton <Eilon@users.noreply.github.com>
2019-10-22 10:43:27 -0700

    Add Image control
    Fixes #10

---
+++ src/Microsoft.Blazor.Native/Elements/Handlers/ImageHandler.cs
@@ -0,0 +1,36 @@
+﻿using Emblazon;
+using System;
+using Xamarin.Forms;
+using XF = Xamarin.Forms;
+
+namespace Microsoft.Blazor.Native.Elements.Handlers
+{
+    public class ImageHandler : ViewHandler
+    {
+        public ImageHandler(EmblazonRenderer renderer, XF.Image imageControl) : base(renderer, imageControl)
+        {
+            ImageControl = imageControl ?? throw new ArgumentNullException(nameof(imageControl));
+        }
+
+        public XF.Image ImageControl { get; }
+
+        public override void ApplyAttribute(ulong attributeEventHandlerId, string attributeName, object attributeValue, string attributeEventUpdatesAttributeName)
+        {
+            switch (attributeName)
+            {
+                case nameof(XF.Image.Aspect):
+                    ImageControl.Aspect = (Aspect)AttributeHelper.GetInt(attributeValue);
+                    break;
+                case nameof(XF.Image.IsOpaque):
+                    ImageControl.IsOpaque = AttributeHelper.GetBool(attributeValue);
+                    break;
+                case nameof(XF.Image.Source) + "_AsFile":
+                    ImageControl.Source = new XF.FileImageSource { File = (string)attributeValue };
+                    break;
+                default:
+                    base.ApplyAttribute(attributeEventHandlerId, attributeName, attributeValue, attributeEventUpdatesAttributeName);
+                    break;
+            }
+        }
+    }
+}
---
+++ src/Microsoft.Blazor.Native/Elements/Image.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Blazor.Native.Elements.Handlers;
+using Emblazon;
+using Microsoft.AspNetCore.Components;
+using System;
+using XF = Xamarin.Forms;
+
+namespace Microsoft.Blazor.Native.Elements
+{
+    public class Image : View
+    {
+        static Image()
+        {
+            ElementHandlerRegistry.RegisterElementHandler<Image>(
+                renderer => new ImageHandler(renderer, new XF.Image()));
+        }
+
+        [Parameter] public XF.Aspect? Aspect { get; set; }
+        [Parameter] public bool? IsOpaque { get; set; }
+        [Parameter] public XF.ImageSource Source { get; set; }
+
+        protected override void RenderAttributes(AttributesBuilder builder)
+        {
+            base.RenderAttributes(builder);
+
+            if (Aspect != null)
+            {
+                builder.AddAttribute(nameof(Aspect), (int)Aspect.Value);
+            }
+            if (IsOpaque != null)
+            {
+                builder.AddAttribute(nameof(IsOpaque), IsOpaque.Value);
+            }
+            if (Source != null)
+            {
+                switch (Source)
+                {
+                    case XF.FileImageSource fileImageSource:
+                        {
+                            builder.AddAttribute(nameof(Source) + "_AsFile", fileImageSource.File);
+                        }
+                        break;
+                    default:
+                        throw new NotSupportedException($"Unsupported {nameof(Source)} type: {Source.GetType().FullName}.");
+                }
+            }
+        }
+    }
+}
