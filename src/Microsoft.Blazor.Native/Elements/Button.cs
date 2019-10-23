0b93dfb98f8239449f20309bbb8e342b9e7800b3
Eilon Lipton <Eilon@users.noreply.github.com>
2019-10-23 16:19:39 -0700

    Code cleanup

--- src/Microsoft.Blazor.Native/Elements/Button.cs
+++ src/Microsoft.Blazor.Native/Elements/Button.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.Blazor.Native.Elements.Handlers;
-using Emblazon;
+﻿using Emblazon;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Blazor.Native.Elements.Handlers;
 using XF = Xamarin.Forms;
 
 namespace Microsoft.Blazor.Native.Elements
