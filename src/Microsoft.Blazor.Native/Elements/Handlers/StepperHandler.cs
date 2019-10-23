bdbbc50d959e7229ca78d5165773ef242b374c71
Eilon Lipton <Eilon@users.noreply.github.com>
2019-10-22 17:04:00 -0700

    Add Stepper control

---
+++ src/Microsoft.Blazor.Native/Elements/Handlers/StepperHandler.cs
@@ -0,0 +1,51 @@
+﻿using Emblazon;
+using Microsoft.AspNetCore.Components;
+using System;
+using XF = Xamarin.Forms;
+
+namespace Microsoft.Blazor.Native.Elements.Handlers
+{
+    public class StepperHandler : ViewHandler
+    {
+        public StepperHandler(EmblazonRenderer renderer, XF.Stepper stepperControl) : base(renderer, stepperControl)
+        {
+            StepperControl = stepperControl ?? throw new ArgumentNullException(nameof(stepperControl));
+            StepperControl.ValueChanged += (s, e) =>
+            {
+                if (ValueChangedEventHandlerId != default)
+                {
+                    renderer.Dispatcher.InvokeAsync(() => renderer.DispatchEventAsync(ValueChangedEventHandlerId, null, new ChangeEventArgs { Value = StepperControl.Value }));
+                }
+            };
+        }
+
+        public ulong ValueChangedEventHandlerId { get; set; }
+        public XF.Stepper StepperControl { get; }
+
+        public override void ApplyAttribute(ulong attributeEventHandlerId, string attributeName, object attributeValue, string attributeEventUpdatesAttributeName)
+        {
+            switch (attributeName)
+            {
+                case nameof(XF.Stepper.Increment):
+                    StepperControl.Increment = AttributeHelper.StringToDouble((string)attributeValue);
+                    break;
+                case nameof(XF.Stepper.Maximum):
+                    StepperControl.Maximum = AttributeHelper.StringToDouble((string)attributeValue);
+                    break;
+                case nameof(XF.Stepper.Minimum):
+                    StepperControl.Minimum = AttributeHelper.StringToDouble((string)attributeValue);
+                    break;
+                case nameof(XF.Stepper.Value):
+                    StepperControl.Value = AttributeHelper.StringToDouble((string)attributeValue);
+                    break;
+                case "onvaluechanged":
+                    Renderer.RegisterEvent(attributeEventHandlerId, () => ValueChangedEventHandlerId = 0);
+                    ValueChangedEventHandlerId = attributeEventHandlerId;
+                    break;
+                default:
+                    base.ApplyAttribute(attributeEventHandlerId, attributeName, attributeValue, attributeEventUpdatesAttributeName);
+                    break;
+            }
+        }
+    }
+}
---
+++ src/Microsoft.Blazor.Native/Elements/Stepper.cs
@@ -0,0 +1,53 @@
+﻿using Emblazon;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Blazor.Native.Elements.Handlers;
+using System.Threading.Tasks;
+using XF = Xamarin.Forms;
+
+namespace Microsoft.Blazor.Native.Elements
+{
+    public class Stepper : View
+    {
+        static Stepper()
+        {
+            ElementHandlerRegistry.RegisterElementHandler<Stepper>(
+                renderer => new StepperHandler(renderer, new XF.Stepper()));
+        }
+
+        [Parameter] public double? Increment { get; set; }
+        [Parameter] public double? Maximum { get; set; }
+        [Parameter] public double? Minimum { get; set; }
+        [Parameter] public double? Value { get; set; }
+
+        [Parameter] public EventCallback<double> ValueChanged { get; set; }
+
+        protected override void RenderAttributes(AttributesBuilder builder)
+        {
+            base.RenderAttributes(builder);
+
+            if (Increment != null)
+            {
+                builder.AddAttribute(nameof(Increment), AttributeHelper.DoubleToString(Increment.Value));
+            }
+            if (Maximum != null)
+            {
+                builder.AddAttribute(nameof(Maximum), AttributeHelper.DoubleToString(Maximum.Value));
+            }
+            if (Minimum != null)
+            {
+                builder.AddAttribute(nameof(Minimum), AttributeHelper.DoubleToString(Minimum.Value));
+            }
+            if (Value != null)
+            {
+                builder.AddAttribute(nameof(Value), AttributeHelper.DoubleToString(Value.Value));
+            }
+
+            builder.AddAttribute("onvaluechanged", EventCallback.Factory.Create<ChangeEventArgs>(this, HandleValueChanged));
+        }
+
+        private Task HandleValueChanged(ChangeEventArgs evt)
+        {
+            return ValueChanged.InvokeAsync((double)evt.Value);
+        }
+    }
+}
