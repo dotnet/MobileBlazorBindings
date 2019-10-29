ff7ec61654aa24d45bd055bf325613bdec2c4e3b
Eilon Lipton <Eilon@users.noreply.github.com>
2019-10-29 11:21:57 -0700

    Add item images and FlyoutHeader to Xaminals sample

--- samples/BlazorNativeXaminals/BlazorNativeXaminals.Android/Resources/Resource.designer.cs
+++ samples/BlazorNativeXaminals/BlazorNativeXaminals.Android/Resources/Resource.designer.cs
@@ -5716,82 +5716,121 @@ namespace BlazorNativeXaminals.Droid
 			public const int avd_show_password = 2131165275;
 			
 			// aapt resource value: 0x7F07005C
-			public const int design_bottom_navigation_item_background = 2131165276;
+			public const int back = 2131165276;
 			
 			// aapt resource value: 0x7F07005D
-			public const int design_fab_background = 2131165277;
+			public const int bear = 2131165277;
 			
 			// aapt resource value: 0x7F07005E
-			public const int design_ic_visibility = 2131165278;
+			public const int cat = 2131165278;
 			
 			// aapt resource value: 0x7F07005F
-			public const int design_ic_visibility_off = 2131165279;
+			public const int design_bottom_navigation_item_background = 2131165279;
 			
 			// aapt resource value: 0x7F070060
-			public const int design_password_eye = 2131165280;
+			public const int design_fab_background = 2131165280;
 			
 			// aapt resource value: 0x7F070061
-			public const int design_snackbar_background = 2131165281;
+			public const int design_ic_visibility = 2131165281;
 			
 			// aapt resource value: 0x7F070062
-			public const int ic_mtrl_chip_checked_black = 2131165282;
+			public const int design_ic_visibility_off = 2131165282;
 			
 			// aapt resource value: 0x7F070063
-			public const int ic_mtrl_chip_checked_circle = 2131165283;
+			public const int design_password_eye = 2131165283;
 			
 			// aapt resource value: 0x7F070064
-			public const int ic_mtrl_chip_close_circle = 2131165284;
+			public const int design_snackbar_background = 2131165284;
 			
 			// aapt resource value: 0x7F070065
-			public const int mtrl_snackbar_background = 2131165285;
+			public const int dog = 2131165285;
 			
 			// aapt resource value: 0x7F070066
-			public const int mtrl_tabs_default_indicator = 2131165286;
+			public const int elephant = 2131165286;
 			
 			// aapt resource value: 0x7F070067
-			public const int navigation_empty_icon = 2131165287;
+			public const int help = 2131165287;
 			
 			// aapt resource value: 0x7F070068
-			public const int notification_action_background = 2131165288;
+			public const int ic_mtrl_chip_checked_black = 2131165288;
 			
 			// aapt resource value: 0x7F070069
-			public const int notification_bg = 2131165289;
+			public const int ic_mtrl_chip_checked_circle = 2131165289;
 			
 			// aapt resource value: 0x7F07006A
-			public const int notification_bg_low = 2131165290;
+			public const int ic_mtrl_chip_close_circle = 2131165290;
 			
 			// aapt resource value: 0x7F07006B
-			public const int notification_bg_low_normal = 2131165291;
+			public const int info = 2131165291;
 			
 			// aapt resource value: 0x7F07006C
-			public const int notification_bg_low_pressed = 2131165292;
+			public const int monkey = 2131165292;
 			
 			// aapt resource value: 0x7F07006D
-			public const int notification_bg_normal = 2131165293;
+			public const int mtrl_snackbar_background = 2131165293;
 			
 			// aapt resource value: 0x7F07006E
-			public const int notification_bg_normal_pressed = 2131165294;
+			public const int mtrl_tabs_default_indicator = 2131165294;
 			
 			// aapt resource value: 0x7F07006F
-			public const int notification_icon_background = 2131165295;
+			public const int navigation_empty_icon = 2131165295;
 			
 			// aapt resource value: 0x7F070070
-			public const int notification_template_icon_bg = 2131165296;
+			public const int notification_action_background = 2131165296;
 			
 			// aapt resource value: 0x7F070071
-			public const int notification_template_icon_low_bg = 2131165297;
+			public const int notification_bg = 2131165297;
 			
 			// aapt resource value: 0x7F070072
-			public const int notification_tile_bg = 2131165298;
+			public const int notification_bg_low = 2131165298;
 			
 			// aapt resource value: 0x7F070073
-			public const int notify_panel_notification_icon_bg = 2131165299;
+			public const int notification_bg_low_normal = 2131165299;
 			
 			// aapt resource value: 0x7F070074
-			public const int tooltip_frame_dark = 2131165300;
+			public const int notification_bg_low_pressed = 2131165300;
 			
 			// aapt resource value: 0x7F070075
-			public const int tooltip_frame_light = 2131165301;
+			public const int notification_bg_normal = 2131165301;
+			
+			// aapt resource value: 0x7F070076
+			public const int notification_bg_normal_pressed = 2131165302;
+			
+			// aapt resource value: 0x7F070077
+			public const int notification_icon_background = 2131165303;
+			
+			// aapt resource value: 0x7F070078
+			public const int notification_template_icon_bg = 2131165304;
+			
+			// aapt resource value: 0x7F070079
+			public const int notification_template_icon_low_bg = 2131165305;
+			
+			// aapt resource value: 0x7F07007A
+			public const int notification_tile_bg = 2131165306;
+			
+			// aapt resource value: 0x7F07007B
+			public const int notify_panel_notification_icon_bg = 2131165307;
+			
+			// aapt resource value: 0x7F07007C
+			public const int paw = 2131165308;
+			
+			// aapt resource value: 0x7F07007D
+			public const int photo = 2131165309;
+			
+			// aapt resource value: 0x7F07007E
+			public const int random = 2131165310;
+			
+			// aapt resource value: 0x7F07007F
+			public const int tooltip_frame_dark = 2131165311;
+			
+			// aapt resource value: 0x7F070080
+			public const int tooltip_frame_light = 2131165312;
+			
+			// aapt resource value: 0x7F070082
+			public const int xamarinstore = 2131165314;
+			
+			// aapt resource value: 0x7F070081
+			public const int xamarin_logo = 2131165313;
 			
 			static Drawable()
 			{
