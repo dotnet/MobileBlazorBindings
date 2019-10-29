1e877d3b4cb127d8678381a826d5e4e47a27b7ee
Eilon Lipton <Eilon@users.noreply.github.com>
2019-10-29 16:30:51 -0700

    Add animal pages and About pages to Xaminals sample

---
+++ samples/BlazorNativeXaminals/BlazorNativeXaminals/Models/Animal.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorNativeXaminals.Models
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Details { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
