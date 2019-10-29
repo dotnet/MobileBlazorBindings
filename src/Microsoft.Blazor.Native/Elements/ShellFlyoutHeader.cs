5f1a52f464ff60fe8fc6e95e070829597be74141
Eilon Lipton <Eilon@users.noreply.github.com>
2019-10-29 13:26:21 -0700

    Suppress incorrect CA1812 analyzer warning

--- src/Microsoft.Blazor.Native/Elements/ShellFlyoutHeader.cs
+++ src/Microsoft.Blazor.Native/Elements/ShellFlyoutHeader.cs
@@ -6,7 +6,9 @@ namespace Microsoft.Blazor.Native.Elements
     // TODO: The base class for this type isn't quite right. It should be something more like View, because that's
     // what Shell.FlyoutHeader requires. But View doesn't have a public ctor and isn't useful on its own, so
     // ContentView is used, which has a settable Content property.
+#pragma warning disable CA1812 // Internal class that is apparently never instantiated; this class is instantiated generically
     internal class ShellFlyoutHeader : ContentView
+#pragma warning restore CA1812 // Internal class that is apparently never instantiated
     {
         static ShellFlyoutHeader()
         {
