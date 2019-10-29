d5a97eb9590e4d93ac9b92cdd5eafd806080c023
Eilon Lipton <Eilon@users.noreply.github.com>
2019-10-29 15:56:50 -0700

    Add support for UriImageSource for images

--- src/Microsoft.Blazor.Native/Elements/AttributeHelper.ImageSource.cs
+++ src/Microsoft.Blazor.Native/Elements/AttributeHelper.ImageSource.cs
@@ -6,7 +6,8 @@ namespace Microsoft.Blazor.Native.Elements
 {
     public static partial class AttributeHelper
     {
-        private const string ImageFileSourcePrefix = "file:";
+        private const string FileImageSourcePrefix = "file";
+        private const string UriImageSourcePrefix = "uri";
 
         /// <summary>
         /// Helper method to serialize <see cref="ImageSource" /> objects.
@@ -20,7 +21,8 @@ namespace Microsoft.Blazor.Native.Elements
 
             return imageSource switch
             {
-                FileImageSource fileImageSource => string.Format(CultureInfo.InvariantCulture, "file:{0}", fileImageSource.File),
+                FileImageSource fileImageSource => string.Format(CultureInfo.InvariantCulture, "{0}:{1}", FileImageSourcePrefix, fileImageSource.File),
+                UriImageSource uriImageSource => string.Format(CultureInfo.InvariantCulture, "{0}:{1}", UriImageSourcePrefix, uriImageSource.Uri.ToString()),
                 _ => throw new NotSupportedException($"Unsupported ImageSource type: {imageSource.GetType().FullName}."),
             };
         }
@@ -35,15 +37,21 @@ namespace Microsoft.Blazor.Native.Elements
                 throw new ArgumentNullException(nameof(imageSourceString));
             }
 
-            if (imageSourceString.StartsWith(ImageFileSourcePrefix, StringComparison.OrdinalIgnoreCase))
+            var indexOfColon = imageSourceString.IndexOf(':');
+            if (indexOfColon == -1)
             {
-                var imageFile = imageSourceString.Substring(ImageFileSourcePrefix.Length);
-                return new FileImageSource { File = imageFile };
+                throw new ArgumentException($"Invalid image source format: '{imageSourceString}'", nameof(imageSourceString));
             }
-            else
+
+            var prefix = imageSourceString.Substring(0, indexOfColon);
+            var data = imageSourceString.Substring(indexOfColon + 1);
+
+            return prefix switch
             {
-                throw new NotSupportedException($"Unsupported ImageSource value: {imageSourceString}");
-            }
+                FileImageSourcePrefix => new FileImageSource { File = data },
+                UriImageSourcePrefix => new UriImageSource { Uri = new Uri(data) },
+                _ => throw new NotSupportedException($"Unsupported ImageSource value: {imageSourceString}"),
+            };
         }
     }
 }
