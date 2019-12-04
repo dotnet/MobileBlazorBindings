584abbd4fafae699bb224c6246834e4a93f7ae99
Eilon Lipton <Eilon@users.noreply.github.com>
2019-12-04 08:58:06 -0800

    Remove unused scripts

--- templates/install.cmd
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-SETLOCAL
-PowerShell -NoProfile -NoLogo -ExecutionPolicy Bypass -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = ''; try { & '%~dp0install.ps1' %*; exit $LASTEXITCODE } catch { write-host $_; exit 1 }"
-SET exit_code=%ERRORLEVEL%
-ECHO install.cmd completed
-EXIT /b %exit_code%
