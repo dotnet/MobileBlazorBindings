446e1222a9b5cff11bdfb097ebb3dce5145613d8
Eilon Lipton <Eilon@users.noreply.github.com>
2020-01-21 14:32:00 -0800

    Add MobileBlazorBindingsHost that works on all platforms
    - In particular, it removes some configuration and logging stuff that's in the default Host
    
    Fixes #30

---
+++ src/Microsoft.MobileBlazorBindings/MobileBlazorBindingsHost.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace Microsoft.MobileBlazorBindings
+{
+    public static class MobileBlazorBindingsHost
+    {
+        public static IHostBuilder CreateDefaultBuilder()
+        {
+            // Inspired by Microsoft.Extensions.Hosting.Host, which can be seen here:
+            // https://github.com/dotnet/extensions/blob/master/src/Hosting/Hosting/src/Host.cs
+            // But slightly modified to work on all of Android, iOS, and UWP.
+
+            var builder = new HostBuilder();
+
+            builder.UseContentRoot(Directory.GetCurrentDirectory());
+
+            builder.ConfigureLogging((hostingContext, logging) =>
+            {
+                logging.AddConsole(configure => configure.DisableColors = true);
+                logging.AddDebug();
+                logging.AddEventSourceLogger();
+            })
+            .UseDefaultServiceProvider((context, options) =>
+            {
+                var isDevelopment = context.HostingEnvironment.IsDevelopment();
+                options.ValidateScopes = isDevelopment;
+                options.ValidateOnBuild = isDevelopment;
+            });
+
+            return builder;
+        }
+    }
+}
