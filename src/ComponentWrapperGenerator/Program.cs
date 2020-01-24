1586482c8c7ef6d6ec0b6d381bd2b78ff0fd1e48
Eilon Lipton <Eilon@users.noreply.github.com>
2020-01-23 17:03:06 -0800

    Add command line arg to specify type to generate

--- src/ComponentWrapperGenerator/Program.cs
+++ src/ComponentWrapperGenerator/Program.cs
@@ -1,13 +1,23 @@
-﻿namespace ComponentWrapperGenerator
+﻿using System;
+
+namespace ComponentWrapperGenerator
 {
     internal class Program
     {
 #pragma warning disable IDE0060 // Remove unused parameter
 #pragma warning disable CA1801 // Parameter is never used
-        internal static void Main(string[] args)
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "<Pending>")]
+        internal static int Main(string[] args)
 #pragma warning restore CA1801 // Parameter is never used
 #pragma warning restore IDE0060 // Remove unused parameter
         {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: dotnet run TypeName");
+                Console.WriteLine("    TypeName must be in the Xamarin.Forms namespace in the Xamarin.Forms assembly.");
+                return -1;
+            }
+
             var settings = new GeneratorSettings
             {
                 FileHeader = @"// Copyright (c) Microsoft Corporation.
@@ -16,9 +26,11 @@
                 RootNamespace = "Microsoft.MobileBlazorBindings.Elements",
             };
 
-            var typeToGenerate = typeof(Xamarin.Forms.Entry);
+            var typeToGenerate = typeof(Xamarin.Forms.Element).Assembly.GetType("Xamarin.Forms." + args[0]);
             var generator = new ComponentWrapperGenerator(settings);
             generator.GenerateComponentWrapper(typeToGenerate);
+
+            return 0;
         }
     }
 }
