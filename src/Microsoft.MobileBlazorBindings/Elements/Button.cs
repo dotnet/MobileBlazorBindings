bfbb75f9e351103cf1b5ed4912c5ad951dc9c052
Eilon Lipton <Eilon@users.noreply.github.com>
2020-01-30 14:53:37 -0800

    Add missing Button events: OnPress and OnRelease

--- src/Microsoft.MobileBlazorBindings/Elements/Button.cs
+++ src/Microsoft.MobileBlazorBindings/Elements/Button.cs
@@ -9,10 +9,14 @@ namespace Microsoft.MobileBlazorBindings.Elements
     public partial class Button : View
     {
         [Parameter] public EventCallback OnClick { get; set; }
+        [Parameter] public EventCallback OnPress { get; set; }
+        [Parameter] public EventCallback OnRelease { get; set; }
 
         partial void RenderAdditionalAttributes(AttributesBuilder builder)
         {
             builder.AddAttribute("onclick", OnClick);
+            builder.AddAttribute("onpress", OnPress);
+            builder.AddAttribute("onrelease", OnRelease);
         }
     }
 }
--- src/Microsoft.MobileBlazorBindings/Elements/Handlers/ButtonHandler.cs
+++ src/Microsoft.MobileBlazorBindings/Elements/Handlers/ButtonHandler.cs
@@ -20,8 +20,34 @@ namespace Microsoft.MobileBlazorBindings.Elements.Handlers
                     renderer.Dispatcher.InvokeAsync(() => renderer.DispatchEventAsync(ClickEventHandlerId, null, e));
                 }
             };
+
+            RegisterEvent(
+                eventName: "onpress",
+                setId: id => PressEventHandlerId = id,
+                clearId: () => PressEventHandlerId = 0);
+            ButtonControl.Pressed += (s, e) =>
+            {
+                if (PressEventHandlerId != default)
+                {
+                    renderer.Dispatcher.InvokeAsync(() => renderer.DispatchEventAsync(PressEventHandlerId, null, e));
+                }
+            };
+
+            RegisterEvent(
+                eventName: "onrelease",
+                setId: id => ReleaseEventHandlerId = id,
+                clearId: () => ReleaseEventHandlerId = 0);
+            ButtonControl.Released += (s, e) =>
+            {
+                if (ReleaseEventHandlerId != default)
+                {
+                    renderer.Dispatcher.InvokeAsync(() => renderer.DispatchEventAsync(ReleaseEventHandlerId, null, e));
+                }
+            };
         }
 
         public ulong ClickEventHandlerId { get; set; }
+        public ulong PressEventHandlerId { get; set; }
+        public ulong ReleaseEventHandlerId { get; set; }
     }
 }
