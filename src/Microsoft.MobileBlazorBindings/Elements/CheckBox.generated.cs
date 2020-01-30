11427e8190b786121ba0909dc54e4845e0996136
Eilon Lipton <Eilon@users.noreply.github.com>
2020-01-30 14:31:44 -0800

    Add CheckBox component

---
+++ src/Microsoft.MobileBlazorBindings/Elements/CheckBox.generated.cs
@@ -0,0 +1,43 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.MobileBlazorBindings.Core;
+using Microsoft.MobileBlazorBindings.Elements.Handlers;
+using System.Threading.Tasks;
+using XF = Xamarin.Forms;
+
+namespace Microsoft.MobileBlazorBindings.Elements
+{
+    public partial class CheckBox : View
+    {
+        static CheckBox()
+        {
+            ElementHandlerRegistry.RegisterElementHandler<CheckBox>(
+                renderer => new CheckBoxHandler(renderer, new XF.CheckBox()));
+        }
+
+        [Parameter] public XF.Color? Color { get; set; }
+        [Parameter] public bool? IsChecked { get; set; }
+
+        public new XF.CheckBox NativeControl => ((CheckBoxHandler)ElementHandler).CheckBoxControl;
+
+        protected override void RenderAttributes(AttributesBuilder builder)
+        {
+            base.RenderAttributes(builder);
+
+            if (Color != null)
+            {
+                builder.AddAttribute(nameof(Color), AttributeHelper.ColorToString(Color.Value));
+            }
+            if (IsChecked != null)
+            {
+                builder.AddAttribute(nameof(IsChecked), IsChecked.Value);
+            }
+
+            RenderAdditionalAttributes(builder);
+        }
+
+        partial void RenderAdditionalAttributes(AttributesBuilder builder);
+    }
+}
---
+++ src/Microsoft.MobileBlazorBindings/Elements/Handlers/CheckBoxHandler.generated.cs
@@ -0,0 +1,39 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using Microsoft.MobileBlazorBindings.Core;
+using System;
+using XF = Xamarin.Forms;
+
+namespace Microsoft.MobileBlazorBindings.Elements.Handlers
+{
+    public partial class CheckBoxHandler : ViewHandler
+    {
+        public CheckBoxHandler(NativeComponentRenderer renderer, XF.CheckBox checkBoxControl) : base(renderer, checkBoxControl)
+        {
+            CheckBoxControl = checkBoxControl ?? throw new ArgumentNullException(nameof(checkBoxControl));
+
+            Initialize(renderer);
+        }
+
+        partial void Initialize(NativeComponentRenderer renderer);
+
+        public XF.CheckBox CheckBoxControl { get; }
+
+        public override void ApplyAttribute(ulong attributeEventHandlerId, string attributeName, object attributeValue, string attributeEventUpdatesAttributeName)
+        {
+            switch (attributeName)
+            {
+                case nameof(XF.CheckBox.Color):
+                    CheckBoxControl.Color = AttributeHelper.StringToColor((string)attributeValue);
+                    break;
+                case nameof(XF.CheckBox.IsChecked):
+                    CheckBoxControl.IsChecked = AttributeHelper.GetBool(attributeValue);
+                    break;
+                default:
+                    base.ApplyAttribute(attributeEventHandlerId, attributeName, attributeValue, attributeEventUpdatesAttributeName);
+                    break;
+            }
+        }
+    }
+}
