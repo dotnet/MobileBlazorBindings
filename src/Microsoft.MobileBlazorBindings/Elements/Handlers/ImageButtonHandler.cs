5f3124cffad19563908f8783aae4da6ee499bbf0
Eilon Lipton <Eilon@users.noreply.github.com>
2020-01-30 14:53:56 -0800

    Add ImageButton component

---
+++ src/Microsoft.MobileBlazorBindings/Elements/Handlers/ImageButtonHandler.cs
@@ -0,0 +1,53 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using Microsoft.MobileBlazorBindings.Core;
+
+namespace Microsoft.MobileBlazorBindings.Elements.Handlers
+{
+    public partial class ImageButtonHandler : ViewHandler
+    {
+        partial void Initialize(NativeComponentRenderer renderer)
+        {
+            RegisterEvent(
+                eventName: "onclick",
+                setId: id => ClickEventHandlerId = id,
+                clearId: () => ClickEventHandlerId = 0);
+            ImageButtonControl.Clicked += (s, e) =>
+            {
+                if (ClickEventHandlerId != default)
+                {
+                    renderer.Dispatcher.InvokeAsync(() => renderer.DispatchEventAsync(ClickEventHandlerId, null, e));
+                }
+            };
+
+            RegisterEvent(
+                eventName: "onpress",
+                setId: id => PressEventHandlerId = id,
+                clearId: () => PressEventHandlerId = 0);
+            ImageButtonControl.Pressed += (s, e) =>
+            {
+                if (PressEventHandlerId != default)
+                {
+                    renderer.Dispatcher.InvokeAsync(() => renderer.DispatchEventAsync(PressEventHandlerId, null, e));
+                }
+            };
+
+            RegisterEvent(
+                eventName: "onrelease",
+                setId: id => ReleaseEventHandlerId = id,
+                clearId: () => ReleaseEventHandlerId = 0);
+            ImageButtonControl.Released += (s, e) =>
+            {
+                if (ReleaseEventHandlerId != default)
+                {
+                    renderer.Dispatcher.InvokeAsync(() => renderer.DispatchEventAsync(ReleaseEventHandlerId, null, e));
+                }
+            };
+        }
+
+        public ulong ClickEventHandlerId { get; set; }
+        public ulong PressEventHandlerId { get; set; }
+        public ulong ReleaseEventHandlerId { get; set; }
+    }
+}
---
+++ src/Microsoft.MobileBlazorBindings/Elements/Handlers/ImageButtonHandler.generated.cs
@@ -0,0 +1,54 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using Microsoft.MobileBlazorBindings.Core;
+using System;
+using XF = Xamarin.Forms;
+
+namespace Microsoft.MobileBlazorBindings.Elements.Handlers
+{
+    public partial class ImageButtonHandler : ViewHandler
+    {
+        public ImageButtonHandler(NativeComponentRenderer renderer, XF.ImageButton imageButtonControl) : base(renderer, imageButtonControl)
+        {
+            ImageButtonControl = imageButtonControl ?? throw new ArgumentNullException(nameof(imageButtonControl));
+
+            Initialize(renderer);
+        }
+
+        partial void Initialize(NativeComponentRenderer renderer);
+
+        public XF.ImageButton ImageButtonControl { get; }
+
+        public override void ApplyAttribute(ulong attributeEventHandlerId, string attributeName, object attributeValue, string attributeEventUpdatesAttributeName)
+        {
+            switch (attributeName)
+            {
+                case nameof(XF.ImageButton.Aspect):
+                    ImageButtonControl.Aspect = (XF.Aspect)AttributeHelper.GetInt(attributeValue);
+                    break;
+                case nameof(XF.ImageButton.BorderColor):
+                    ImageButtonControl.BorderColor = AttributeHelper.StringToColor((string)attributeValue);
+                    break;
+                case nameof(XF.ImageButton.BorderWidth):
+                    ImageButtonControl.BorderWidth = AttributeHelper.StringToDouble((string)attributeValue, -1.00);
+                    break;
+                case nameof(XF.ImageButton.CornerRadius):
+                    ImageButtonControl.CornerRadius = AttributeHelper.GetInt(attributeValue, -1);
+                    break;
+                case nameof(XF.ImageButton.IsOpaque):
+                    ImageButtonControl.IsOpaque = AttributeHelper.GetBool(attributeValue);
+                    break;
+                case nameof(XF.ImageButton.Padding):
+                    ImageButtonControl.Padding = AttributeHelper.StringToThickness(attributeValue);
+                    break;
+                case nameof(XF.ImageButton.Source):
+                    ImageButtonControl.Source = AttributeHelper.StringToImageSource(attributeValue);
+                    break;
+                default:
+                    base.ApplyAttribute(attributeEventHandlerId, attributeName, attributeValue, attributeEventUpdatesAttributeName);
+                    break;
+            }
+        }
+    }
+}
