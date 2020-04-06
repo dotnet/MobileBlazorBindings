390d9b36c06968dd029975d54181b5c7d99af9bf
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-04-06 16:04:29 +0100

    Add WebWindow.Blazor sources

---
+++ WebWindow.Blazor/DesktopApplicationBuilder.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+
+namespace WebWindows.Blazor
+{
+    public class DesktopApplicationBuilder
+    {
+        public DesktopApplicationBuilder(IServiceProvider services)
+        {
+            Services = services;
+            Entries = new List<(Type componentType, string domElementSelector)>();
+        }
+
+        public List<(Type componentType, string domElementSelector)> Entries { get; }
+
+        public IServiceProvider Services { get; }
+
+        public void AddComponent(Type componentType, string domElementSelector)
+        {
+            if (componentType == null)
+            {
+                throw new ArgumentNullException(nameof(componentType));
+            }
+
+            if (domElementSelector == null)
+            {
+                throw new ArgumentNullException(nameof(domElementSelector));
+            }
+
+            Entries.Add((componentType, domElementSelector));
+        }
+
+        public void AddComponent<T>(string domElementSelector) where T : IComponent
+            => AddComponent(typeof(T), domElementSelector);
+    }
+}
