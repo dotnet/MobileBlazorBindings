e617f8cd5f1782cab754a607eb0060b5cf53f854
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-04-08 13:20:12 +0100

    Create WebView MBB component

---
+++ src/BlazorDesktop/Elements/Handlers/WebViewHandler.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.MobileBlazorBindings.Core;
+using Microsoft.MobileBlazorBindings.Elements.Handlers;
+using Xamarin.Forms;
+using XF = Xamarin.Forms;
+
+namespace BlazorDesktop.Elements.Handlers
+{
+    internal class WebViewHandler : ViewHandler
+    {
+        public XF.WebView Control { get; }
+
+        public WebViewHandler(NativeComponentRenderer renderer, XF.WebView control)
+            : base(renderer, control)
+        {
+            Control = control;
+
+            var htmlSource = new HtmlWebViewSource();
+            htmlSource.Html = @"<html><body>
+                                <h1>Trident</h1>
+                                <p>This is an IE11 control.</p>
+                                <script>document.write(navigator.userAgent);</script>
+                                </body>
+                                </html>";
+            control.Source = htmlSource;
+        }
+    }
+}
---
+++ src/BlazorDesktop/Elements/WebView.cs
@@ -0,0 +1,16 @@
+﻿using BlazorDesktop.Elements.Handlers;
+using Microsoft.MobileBlazorBindings.Core;
+using Microsoft.MobileBlazorBindings.Elements;
+using XF = Xamarin.Forms;
+
+namespace BlazorDesktop.Elements
+{
+    public class WebView : View
+    {
+        static WebView()
+        {
+            ElementHandlerRegistry
+                .RegisterElementHandler<WebView>(renderer => new WebViewHandler(renderer, new XF.WebView()));
+        }
+    }
+}
