38c53941cf6574a9edada75ddb31175d8fa204ce
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-04-10 14:56:01 +0100

    Support scheme handlers

--- src/BlazorDesktop.macOS/WebViewExtendedWKWebViewRenderer.cs
+++ src/BlazorDesktop.macOS/WebViewExtendedWKWebViewRenderer.cs
@@ -9,6 +9,7 @@ using System.Text.Json;
 using System.Runtime.Serialization.Json;
 using System.IO;
 using System.Text.Encodings.Web;
+using System;
 
 [assembly: XF.ExportRenderer(typeof(WebViewExtended), typeof(WebViewExtendedWKWebViewRenderer))]
 
@@ -48,6 +49,11 @@ namespace BlazorDesktop.macOS
                     config.UserContentController.AddUserScript(new WKUserScript(
                         new NSString(initScriptSource), WKUserScriptInjectionTime.AtDocumentStart, isForMainFrameOnly: true));
                     config.UserContentController.AddScriptMessageHandler(this, "webwindowinterop");
+
+                    foreach (var (scheme, handler) in Element.SchemeHandlers)
+                    {
+                        config.SetUrlSchemeHandler(new SchemeHandler(handler), scheme);
+                    }
 
                     _wkWebView = new WKWebView(Frame, config);
                     SetNativeControl(_wkWebView);
@@ -104,6 +110,48 @@ namespace BlazorDesktop.macOS
         public void DidReceiveScriptMessage(WKUserContentController userContentController, WKScriptMessage message)
         {
             Element.HandleWebMessageReceived(((NSString)message.Body).ToString());
+        }
+
+        class SchemeHandler : NSObject, IWKUrlSchemeHandler
+        {
+            private ResolveWebResourceDelegate handler;
+
+            public SchemeHandler(ResolveWebResourceDelegate handler)
+            {
+                this.handler = handler;
+            }
+
+            [Export("webView:startURLSchemeTask:")]
+            public void StartUrlSchemeTask(WKWebView webView, IWKUrlSchemeTask urlSchemeTask)
+            {
+                var responseBytes = GetResponseBytes(urlSchemeTask.Request.Url.AbsoluteString, out var contentType, out var statusCode);
+                var response = new NSUrlResponse(urlSchemeTask.Request.Url, contentType, responseBytes.Length, null);
+                urlSchemeTask.DidReceiveResponse(response);
+                urlSchemeTask.DidReceiveData(NSData.FromArray(responseBytes));
+                urlSchemeTask.DidFinish();
+            }
+
+            private byte[] GetResponseBytes(string url, out string contentType, out int statusCode)
+            {
+                var responseStream = handler(url, out contentType);
+                if (responseStream == null)
+                {
+                    statusCode = 404;
+                    return Array.Empty<byte>();
+                }
+                else
+                {
+                    statusCode = 200;
+                    var ms = new MemoryStream();
+                    responseStream.CopyTo(ms);
+                    return ms.ToArray();
+                }
+            }
+
+            [Export("webView:stopURLSchemeTask:")]
+            public void StopUrlSchemeTask(WKWebView webView, IWKUrlSchemeTask urlSchemeTask)
+            {
+            }
         }
     }
 }
