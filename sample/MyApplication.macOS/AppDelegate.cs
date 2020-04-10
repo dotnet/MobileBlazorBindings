68a987844b03ed9528c4d0cd6c24a8eb5a51fabc
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-04-10 11:51:36 +0100

    Now actually runs the Xamarin Forms native parts

--- sample/MyApplication.macOS/AppDelegate.cs
+++ sample/MyApplication.macOS/AppDelegate.cs
@@ -4,15 +4,26 @@ using Foundation;
 namespace MyApplication.macOS
 {
     [Register("AppDelegate")]
-    public class AppDelegate : NSApplicationDelegate
+    public class AppDelegate : Xamarin.Forms.Platform.MacOS.FormsApplicationDelegate
     {
         public AppDelegate()
         {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+            var rect = new CoreGraphics.CGRect(200, 1000, 1024, 768);
+            MainWindow = new NSWindow(rect, style, NSBackingStore.Buffered, false)
+            {
+                Title = "My Application",
+                TitleVisibility = NSWindowTitleVisibility.Visible,
+            };
         }
+
+        public override NSWindow MainWindow { get; }
 
         public override void DidFinishLaunching(NSNotification notification)
         {
-            // Insert code here to initialize your application
+            Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
         }
 
         public override void WillTerminate(NSNotification notification)
--- sample/MyApplication.macOS/Main.cs
+++ sample/MyApplication.macOS/Main.cs
@@ -7,6 +7,7 @@ namespace MyApplication.macOS
         static void Main(string[] args)
         {
             NSApplication.Init();
+            NSApplication.SharedApplication.Delegate = new AppDelegate();
             NSApplication.Main(args);
         }
     }
