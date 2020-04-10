d1ecb08a0eb9c2dffa0e4f11b446b00821070829
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-04-10 12:32:33 +0100

    Now can load arbitrary URLs/HTML

--- src/BlazorDesktop.macOS/WebViewExtendedWKWebViewRenderer.cs
+++ src/BlazorDesktop.macOS/WebViewExtendedWKWebViewRenderer.cs
@@ -2,31 +2,67 @@
 using BlazorDesktop.macOS;
 using Foundation;
 using WebKit;
-using Xamarin.Forms;
+using XF = Xamarin.Forms;
 using Xamarin.Forms.Platform.MacOS;
+using System.ComponentModel;
 
-[assembly: ExportRenderer(typeof(WebViewExtended), typeof(WebViewExtendedWKWebViewRenderer))]
+[assembly: XF.ExportRenderer(typeof(WebViewExtended), typeof(WebViewExtendedWKWebViewRenderer))]
 
 namespace BlazorDesktop.macOS
 {
-    public class WebViewExtendedWKWebViewRenderer : ViewRenderer<WebViewExtended, WKWebView>
+    public class WebViewExtendedWKWebViewRenderer : ViewRenderer<WebViewExtended, WKWebView>, XF.IWebViewDelegate
     {
         WKWebView _wkWebView;
 
         protected override void OnElementChanged(ElementChangedEventArgs<WebViewExtended> e)
         {
-            base.OnElementChanged(e);
-
-            if (Control == null)
-            {
-                var config = new WKWebViewConfiguration();
-                _wkWebView = new WKWebView(Frame, config);
-                SetNativeControl(_wkWebView);
-            }
             if (e.NewElement != null)
             {
-                Control.LoadRequest(new NSUrlRequest(new NSUrl("https://microsoft.com")));
+                if (Control == null)
+                {
+                    var config = new WKWebViewConfiguration();
+                    _wkWebView = new WKWebView(Frame, config);
+                    SetNativeControl(_wkWebView);
+                }
+
+                Load();
             }
+
+            base.OnElementChanged(e);
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName == XF.WebView.SourceProperty.PropertyName)
+            {
+                Load();
+            }
+        }
+
+        void Load()
+        {
+            if (Element.Source != null)
+            {
+                Element.Source.Load(this);
+            }
+        }
+
+        public void LoadHtml(string html, string baseUrl)
+        {
+            if (html == null)
+                return;
+
+            Control.LoadHtmlString(html, new NSUrl(baseUrl ?? "about:blank"));
+        }
+
+        public void LoadUrl(string url)
+        {
+            if (url == null)
+                return;
+
+            Control.LoadRequest(new NSUrlRequest(new NSUrl(url)));
         }
     }
 }
