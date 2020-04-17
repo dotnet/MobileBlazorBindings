ba09fbb1ebdf772902377036df3853e4e5f47014
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-04-17 15:26:23 +0100

    Begin adding MessageApp sample

---
+++ sample/MessageApp/MessageApp/Models/HomeMenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessageApp.Models
+{
+    public enum MenuItemType
+    {
+        Browse,
+        About
+    }
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
