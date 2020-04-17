36957d4bf7527f4611205dbe50df8c709a916dc6
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-04-17 18:04:21 +0100

    Load data from json file

---
+++ sample/MessageApp/MessageApp/Data/MailFolder.cs
@@ -0,0 +1,8 @@
+﻿namespace MessageApp.Data
+{
+    public class MailFolder
+    {
+        public string Id { get; set; }
+        public Message[] Mails { get; set; }
+    }
+}
---
+++ sample/MessageApp/MessageApp/Data/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MessageApp.Data
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Date { get; set; }
+        public string Subject { get; set; }
+    }
+}
