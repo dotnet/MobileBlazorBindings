1a29a5075dad47853e5ca5fb8efba451d779b636
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-04-17 19:20:09 +0100

    Tweaks to make it run on iOS

--- sample/MessageApp/MessageApp.iOS/AppDelegate.cs
+++ sample/MessageApp/MessageApp.iOS/AppDelegate.cs
@@ -23,7 +23,7 @@ namespace MessageApp.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
-            LoadApplication(new App());
+            LoadApplication(new MessageApp.MainPage());
 
             return base.FinishedLaunching(app, options);
         }
