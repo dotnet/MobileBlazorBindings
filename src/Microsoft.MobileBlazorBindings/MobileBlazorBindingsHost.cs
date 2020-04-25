0bffddf6a04a016d99d94d3e4239bd4e417acf3d
Eilon Lipton <Eilon@users.noreply.github.com>
2020-04-25 14:25:03 -0700

    Fix CSS support for iOS apps #109
    Fixes #109

--- src/Microsoft.MobileBlazorBindings/MobileBlazorBindingsHost.cs
+++ src/Microsoft.MobileBlazorBindings/MobileBlazorBindingsHost.cs
@@ -32,7 +32,22 @@ namespace Microsoft.MobileBlazorBindings
                 options.ValidateOnBuild = isDevelopment;
             });
 
+            EnableStyleSheetSupport();
+
             return builder;
+        }
+
+        private static void EnableStyleSheetSupport()
+        {
+            // Force the Xamarin.Forms.Xaml assembly to load, which will cause the style sheet dependency
+            // to be made available to Xamarin.Forms' Dependency Injection service. DI services are
+            // made available by scanning loaded assemblies for various attributes, but sometimes this
+            // assembly isn't yet loaded, meaning the CSS StyleSheet loader service won't be available
+            // to be discovered.
+            // See these issues for more info:
+            // - https://github.com/xamarin/Xamarin.Forms/issues/7775
+            // - https://github.com/xamarin/MobileBlazorBindings/issues/82#issuecomment-618997676
+            _ = typeof(Xamarin.Forms.Xaml.StyleSheetExtension);
         }
     }
 }
