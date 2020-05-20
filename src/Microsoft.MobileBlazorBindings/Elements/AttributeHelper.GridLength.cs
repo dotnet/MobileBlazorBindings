a388f7cc0b3599c1cd7e4a1cf35492dad357a8bf
Eilon Lipton <Eilon@users.noreply.github.com>
2020-05-20 16:31:59 -0700

    Add support for XF.GridLength in attribute helpers

---
+++ src/Microsoft.MobileBlazorBindings/Elements/AttributeHelper.GridLength.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Microsoft.MobileBlazorBindings.Elements
+{
+    public static partial class AttributeHelper
+    {
+        /// <summary>
+        /// Helper method to serialize <see cref="GridLength" /> objects.
+        /// </summary>
+        public static string GridLengthToString(GridLength gridLength)
+        {
+            return string.Format(
+                CultureInfo.InvariantCulture,
+                "{0:R},{1}", // "R" --> Round-trip format specifier guarantees fidelity when parsing System.Double
+                gridLength.Value,
+                (int)gridLength.GridUnitType);
+        }
+
+        /// <summary>
+        /// Helper method to deserialize <see cref="GridLength" /> objects.
+        /// </summary>
+        public static GridLength StringToGridLength(object gridLengthString, GridLength defaultValueIfNull = default)
+        {
+            if (gridLengthString is null)
+            {
+                return defaultValueIfNull;
+            }
+            if (!(gridLengthString is string gridLengthAsString))
+            {
+                throw new ArgumentNullException(nameof(gridLengthString));
+            }
+
+            var stringParts = gridLengthAsString.Split(',');
+
+            if (stringParts.Length != 2)
+            {
+                throw new ArgumentNullException(nameof(gridLengthString), message: "Expected value to have one comma (',') in it.");
+            }
+
+            return
+                new GridLength(
+                    double.Parse(stringParts[0], CultureInfo.InvariantCulture),
+                    (GridUnitType)int.Parse(stringParts[1], CultureInfo.InvariantCulture));
+        }
+    }
+}
