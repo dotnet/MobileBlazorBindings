b6a8c62045790bc57a15cecfcfd566a5f1ff696d
Eilon Lipton <Eilon@users.noreply.github.com>
2020-06-25 10:18:02 -0700

    Add doc comments for Init methods

--- src/Microsoft.MobileBlazorBindings.WebView.Windows/BlazorHybridWindows.cs
+++ src/Microsoft.MobileBlazorBindings.WebView.Windows/BlazorHybridWindows.cs
@@ -2,9 +2,13 @@
 {
     public static class BlazorHybridWindows
     {
-        // Calling this means the assembly will be loaded, so XF will discover its ExportRenderer attributes
+        /// <summary>
+        /// Ensures the initialization of required features for Blazor Hybrid applications.
+        /// This method should be called as early as possible in the application startup logic.
+        /// </summary>
         public static void Init()
         {
+            // Calling this means the assembly will be loaded, so Xamarin.Forms will discover its ExportRenderer attributes
         }
     }
 }
