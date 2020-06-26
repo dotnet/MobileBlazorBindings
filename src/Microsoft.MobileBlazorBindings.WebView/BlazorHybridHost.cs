052560281b2dae9bef810f4da011c76fa48e1d0c
Eilon Lipton <Eilon@users.noreply.github.com>
2020-06-25 22:18:05 -0700

    Merge Blazor Hybrid projects into Mobile Blazor Bindings solution
    - Update min SDK to .NET Core 3.1

--- src/Microsoft.MobileBlazorBindings.WebView/BlazorHybridHost.cs
+++ src/Microsoft.MobileBlazorBindings.WebView/BlazorHybridHost.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 
 namespace Microsoft.MobileBlazorBindings.WebView
 {
-    public class BlazorHybridHost
+    public class static BlazorHybridHost
     {
         private static readonly List<Lazy<IFileProvider>> _resourceFileProviders = new List<Lazy<IFileProvider>>();
 
