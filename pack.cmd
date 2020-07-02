e472726a2e30d949183ea624a55ffc04c139e1d3
Eilon Lipton <Eilon@users.noreply.github.com>
2020-07-02 16:51:27 -0700

    Update pack.cmd

--- pack.cmd
+++ pack.cmd
@@ -3,11 +3,5 @@ setlocal
 
 set THIS_DIR=%~dp0
 
-echo --- Microsoft.MobileBlazorBindings.Core ---
-msbuild /t:pack %* %THIS_DIR%\src\Microsoft.MobileBlazorBindings.Core\Microsoft.MobileBlazorBindings.Core.csproj
-echo --- BlinForms ---
-msbuild /t:pack %* %THIS_DIR%\src\BlinForms.Framework\BlinForms.Framework.csproj
-echo --- MobileBlazorBindings ---
-msbuild /t:pack %* %THIS_DIR%\src\Microsoft.MobileBlazorBindings\Microsoft.MobileBlazorBindings.csproj
-echo --- MobileBlazorBindings Templates ---
-msbuild /t:pack %* %THIS_DIR%\templates\MobileBlazorBindings-template-pack.csproj
+echo --- Packing all the things! ---
+msbuild /t:pack %* %THIS_DIR%\CI\CI.Windows.proj
