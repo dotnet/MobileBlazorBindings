0effaabc2e915bd3280b955527ba51b6281d1896
Eilon Lipton <Eilon@users.noreply.github.com>
2020-07-07 11:08:01 -0700

    Add Android version of HybridMessageApp

---
+++ samples/HybridMessageApp/HybridMessageApp.Android/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Microsoft.MobileBlazorBindings.WebView.Android;
+
+namespace HybridMessageApp.Droid
+{
+    [Activity(Label = "HybridMessageApp.Android", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            BlazorHybridAndroid.Init();
+         
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new MainPage());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
