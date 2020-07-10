f807b334c721a4c92e26993ef40a0f9d7cd4d8b2
Eilon Lipton <Eilon@users.noreply.github.com>
2020-07-10 16:24:00 -0700

    Add Hybrid App project template - Also fixes several build issues by creatively updating various MSBuild targets

--- pack.cmd
+++ pack.cmd
@@ -4,4 +4,11 @@ setlocal
 set THIS_DIR=%~dp0
 
 echo --- Building and packing all the things! ---
-msbuild /t:clean;restore;build;pack %* %THIS_DIR%\CI\CI.Windows.proj
+
+msbuild /t:clean %* %THIS_DIR%\CI\CI.Windows.proj
+
+msbuild /t:restore %* %THIS_DIR%\CI\CI.Windows.proj
+
+msbuild /t:build %* %THIS_DIR%\CI\CI.Windows.proj
+
+msbuild /t:pack %* %THIS_DIR%\CI\CI.Windows.proj
---
+++ templates/BlazorHybrid-app/NewApp/App.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.MobileBlazorBindings;
+using Microsoft.MobileBlazorBindings.WebView;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace NewApp
+{
+    public class App : Application
+    {
+        public App()
+        {
+            BlazorHybridHost.AddResourceAssembly(GetType().Assembly, contentRoot: "WebUI/wwwroot");
+
+            var host = MobileBlazorBindingsHost.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    // Adds web-specific services such as NavigationManager
+                    services.AddBlazorHybrid();
+
+                    // Register app-specific services
+                    services.AddSingleton<CounterState>();
+                })
+                .Build();
+
+            MainPage = new ContentPage { Title = "My Application" };
+            host.AddComponent<Main>(parent: MainPage);
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
