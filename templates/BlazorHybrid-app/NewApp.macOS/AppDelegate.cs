33d3bdfdcdcf18c154d5c40ae77768c6e22f467e
Eilon Lipton <Eilon@users.noreply.github.com>
2020-07-14 23:57:37 -0700

    Add macOS and Windows projects for HybridApp template

---
+++ templates/BlazorHybrid-app/NewApp.macOS/AppDelegate.cs
@@ -0,0 +1,62 @@
+﻿using AppKit;
+using Foundation;
+
+namespace NewApp.macOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : Xamarin.Forms.Platform.MacOS.FormsApplicationDelegate
+    {
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+            var rect = new CoreGraphics.CGRect(200, 1000, 1024, 768);
+            MainWindow = new NSWindow(rect, style, NSBackingStore.Buffered, false)
+            {
+                Title = "My Application",
+                TitleVisibility = NSWindowTitleVisibility.Visible,
+            };
+        }
+
+        public override NSWindow MainWindow { get; }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            // Menu options to make it easy to press cmd+q to quit the app
+            NSApplication.SharedApplication.MainMenu = MakeMainMenu();
+
+            Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+
+        private NSMenu MakeMainMenu()
+        {
+            // top bar app menu
+            var menubar = new NSMenu();
+            var appMenuItem = new NSMenuItem();
+            menubar.AddItem(appMenuItem);
+
+            var appMenu = new NSMenu();
+            appMenuItem.Submenu = appMenu;
+
+            // add separator
+            var separator = NSMenuItem.SeparatorItem;
+            appMenu.AddItem(separator);
+
+            // add quit menu item
+            var quitTitle = string.Format("Quit {0}", "NewApp.macOS");
+            var quitMenuItem = new NSMenuItem(quitTitle, "q", delegate
+            {
+                NSApplication.SharedApplication.Terminate(menubar);
+            });
+            appMenu.AddItem(quitMenuItem);
+
+            return menubar;
+        }
+    }
+}
---
+++ templates/BlazorHybrid-app/NewApp.macOS/Main.cs
@@ -0,0 +1,16 @@
+﻿using AppKit;
+using Microsoft.MobileBlazorBindings.WebView.macOS;
+
+namespace NewApp.macOS
+{
+    internal static class MainClass
+    {
+        private static void Main(string[] args)
+        {
+            BlazorHybridMacOS.Init();
+            NSApplication.Init();
+            NSApplication.SharedApplication.Delegate = new AppDelegate();
+            NSApplication.Main(args);
+        }
+    }
+}
