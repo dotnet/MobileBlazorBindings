c59c1d64ecaf83dde530b682cf56dbd46b560931
Eilon Lipton <Eilon@users.noreply.github.com>
2020-07-22 13:41:20 -0700

    Fix CSS on iOS for real
    Fixes #141

--- src/Microsoft.MobileBlazorBindings/MobileBlazorBindingsHost.cs
+++ src/Microsoft.MobileBlazorBindings/MobileBlazorBindingsHost.cs
@@ -45,9 +45,15 @@ namespace Microsoft.MobileBlazorBindings
             // assembly isn't yet loaded, meaning the CSS StyleSheet loader service won't be available
             // to be discovered.
             // See these issues for more info:
-            // - https://github.com/xamarin/Xamarin.Forms/issues/7775
-            // - https://github.com/xamarin/MobileBlazorBindings/issues/82#issuecomment-618997676
-            _ = typeof(Xamarin.Forms.Xaml.StyleSheetExtension);
+            // - Xamarin.Forms issue: https://github.com/xamarin/Xamarin.Forms/issues/7775
+            // - MBB issue with previous fix: https://github.com/xamarin/MobileBlazorBindings/issues/82#issuecomment-618997676
+            // - MBB issue with updated fix: https://github.com/xamarin/MobileBlazorBindings/issues/140
+
+            // Note: This code is somewhat arbitrary. The only requirement is that it *runs* code in a type
+            // that's in the Xamarin.Forms.Xaml assembly.
+            // Note: There are many types in the Xamarin.Forms.Xaml namespace that are *not* in that
+            // assembly, so be careful if changing this.
+            _ = new Xamarin.Forms.Xaml.XamlCompilationAttribute(Xamarin.Forms.Xaml.XamlCompilationOptions.Skip);
         }
     }
 }
