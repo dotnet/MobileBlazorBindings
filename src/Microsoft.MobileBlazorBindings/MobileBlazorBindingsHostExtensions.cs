e4a7e9865815879caa84cd1daa527980f6654244
Lachlan Gordon <lachlanwgordon@gmail.com>
2020-07-31 08:59:01 +1000

    Implemented all suggestions in notes on pull request
    Lots of little cleanup tasks.

--- src/Microsoft.MobileBlazorBindings/MobileBlazorBindingsHostExtensions.cs
+++ src/Microsoft.MobileBlazorBindings/MobileBlazorBindingsHostExtensions.cs
@@ -69,8 +69,6 @@ namespace Microsoft.MobileBlazorBindings
                 throw new InvalidOperationException($"Cannot add {type.Name} to {parent.GetType().Name}. {type.Name} is not an IComponent. If you are trying to add an XF type, try adding the MobileBlazorBindings equivalent instead");
             }
 
-            //NOTE: Do not add a using to dispose of this. If you do the page will look like it works but can't be updated after initial render.
-            //TODO: Investigate if we should dispose of this renderer when the XF page dissapears
 #pragma warning disable CA2000 // Dispose objects before losing scope
             var renderer = new MobileBlazorBindingsRenderer(services, services.GetRequiredService<ILoggerFactory>());
 #pragma warning restore CA2000 // Dispose objects before losing scope
