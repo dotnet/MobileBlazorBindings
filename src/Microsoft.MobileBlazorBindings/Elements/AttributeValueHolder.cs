c4c0b1d367f1c6a64c6fba7a919230ed74cb8db9
Eilon Lipton <Eilon@users.noreply.github.com>
2020-08-11 13:18:47 -0700

    Remove now-unused code

--- src/Microsoft.MobileBlazorBindings/Elements/AttributeValueHolder.cs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-namespace Microsoft.MobileBlazorBindings.Elements
-{
-    public delegate void AttributeValueHolder(out object value);
-}
