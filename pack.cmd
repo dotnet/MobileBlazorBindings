7d126e6ddf0a5239988ab9a04b347e32fb85aac5
Eilon Lipton <Eilon@users.noreply.github.com>
2020-08-31 16:16:41 -0700

    WIP: Build stuff

--- pack.cmd
+++ pack.cmd
@@ -7,12 +7,25 @@ echo --- Building and packing all the things! ---
 
 msbuild /t:clean %* %THIS_DIR%\CI\CI.Windows.proj
 
+if errorlevel 1 exit /b %ERRORLEVEL%
+
 msbuild /t:restore %* %THIS_DIR%\CI\CI.Windows.proj
+
+if errorlevel 1 exit /b %ERRORLEVEL%
 
 msbuild /t:build %* %THIS_DIR%\CI\CI.Windows.proj
 
+if errorlevel 1 exit /b %ERRORLEVEL%
+
 msbuild /t:pack %* %THIS_DIR%\CI\CI.Windows.proj
+
+if errorlevel 1 exit /b %ERRORLEVEL%
 
 msbuild /t:restore %* %THIS_DIR%\templates\MobileBlazorBindings-template-pack.csproj
 
+if errorlevel 1 exit /b %ERRORLEVEL%
+
 msbuild /t:pack %* %THIS_DIR%\templates\MobileBlazorBindings-template-pack.csproj
+
+if errorlevel 1 exit /b %ERRORLEVEL%
+
