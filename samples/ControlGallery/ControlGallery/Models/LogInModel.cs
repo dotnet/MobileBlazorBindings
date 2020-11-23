61e949c222067ba1c88fbaff872076cd692d1205
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2020-11-23 19:21:11 +0200

    Add validations playground sample

---
+++ samples/ControlGallery/ControlGallery/Models/LogInModel.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System.ComponentModel.DataAnnotations;
+
+namespace ControlGallery.Models
+{
+    public class LogInModel
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, MinLength(8), MaxLength(16)]
+        public string Password { get; set; }
+
+        [Required, MinLength(8), MaxLength(16)]
+        public string ConfirmPassword { get; set; }
+    }
+}
