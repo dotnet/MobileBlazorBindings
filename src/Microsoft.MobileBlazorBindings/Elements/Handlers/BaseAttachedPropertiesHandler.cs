bef23955ab86162fcd57dd9f6b7875209dac08e9
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2020-12-02 11:39:21 +0200

    Add ShellProperties for attached properties

---
+++ src/Microsoft.MobileBlazorBindings/Elements/Handlers/BaseAttachedPropertiesHandler.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.MobileBlazorBindings.Core;
+using System;
+using XF = Xamarin.Forms;
+
+namespace Microsoft.MobileBlazorBindings.Elements.Handlers
+{
+    public abstract class BaseAttachedPropertiesHandler : IXamarinFormsElementHandler, INonPhysicalChild
+    {
+        protected XF.BindableObject Parent { get; private set; }
+
+        public abstract void ApplyAttribute(ulong attributeEventHandlerId, string attributeName, object attributeValue, string attributeEventUpdatesAttributeName);
+
+        public void SetParent(object parentElement)
+        {
+            Parent = (XF.BindableObject)parentElement;
+        }
+
+        // Because this is a 'fake' element, all matters related to physical trees
+        // should be no-ops.
+        public XF.Element ElementControl => null;
+        public object TargetElement => null;
+        public int GetPhysicalSiblingIndex() => 0;
+        public bool IsParented() => false;
+        public bool IsParentedTo(XF.Element parent) => false;
+
+        public void SetParent(XF.Element parent)
+        {
+            // This should never get called. Instead, INonPhysicalChild.SetParent() implemented
+            // in this class should get called.
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ src/Microsoft.MobileBlazorBindings/Elements/Handlers/ShellPropertiesHandler.cs
@@ -0,0 +1,53 @@
+﻿using XF = Xamarin.Forms;
+
+namespace Microsoft.MobileBlazorBindings.Elements.Handlers
+{
+    public class ShellPropertiesHandler : BaseAttachedPropertiesHandler
+    {
+        public override void ApplyAttribute(ulong attributeEventHandlerId, string attributeName, object attributeValue, string attributeEventUpdatesAttributeName)
+        {
+            switch (attributeName)
+            {
+                case nameof(ShellProperties.NavBarIsVisible):
+                    XF.Shell.SetNavBarIsVisible(Parent, AttributeHelper.GetBool(attributeValue));
+                    break;
+                case nameof(ShellProperties.NavBarHasShadow):
+                    XF.Shell.SetNavBarHasShadow(Parent, AttributeHelper.GetBool(attributeValue));
+                    break;
+                case nameof(ShellProperties.TabBarIsVisible):
+                    XF.Shell.SetTabBarIsVisible(Parent, AttributeHelper.GetBool(attributeValue));
+                    break;
+                case nameof(ShellProperties.BackgroundColor):
+                    XF.Shell.SetBackgroundColor(Parent, AttributeHelper.StringToColor(attributeValue));
+                    break;
+                case nameof(ShellProperties.DisabledColor):
+                    XF.Shell.SetDisabledColor(Parent, AttributeHelper.StringToColor(attributeValue));
+                    break;
+                case nameof(ShellProperties.ForegroundColor):
+                    XF.Shell.SetForegroundColor(Parent, AttributeHelper.StringToColor(attributeValue));
+                    break;
+                case nameof(ShellProperties.TabBarBackgroundColor):
+                    XF.Shell.SetTabBarBackgroundColor(Parent, AttributeHelper.StringToColor(attributeValue));
+                    break;
+                case nameof(ShellProperties.TabBarDisabledColor):
+                    XF.Shell.SetTabBarDisabledColor(Parent, AttributeHelper.StringToColor(attributeValue));
+                    break;
+                case nameof(ShellProperties.TabBarForegroundColor):
+                    XF.Shell.SetTabBarForegroundColor(Parent, AttributeHelper.StringToColor(attributeValue));
+                    break;
+                case nameof(ShellProperties.TabBarTitleColor):
+                    XF.Shell.SetTabBarTitleColor(Parent, AttributeHelper.StringToColor(attributeValue));
+                    break;
+                case nameof(ShellProperties.TabBarUnselectedColor):
+                    XF.Shell.SetTabBarUnselectedColor(Parent, AttributeHelper.StringToColor(attributeValue));
+                    break;
+                case nameof(ShellProperties.TitleColor):
+                    XF.Shell.SetTitleColor(Parent, AttributeHelper.StringToColor(attributeValue));
+                    break;
+                case nameof(ShellProperties.UnselectedColor):
+                    XF.Shell.SetUnselectedColor(Parent, AttributeHelper.StringToColor(attributeValue));
+                    break;
+            }
+        }
+    }
+}
