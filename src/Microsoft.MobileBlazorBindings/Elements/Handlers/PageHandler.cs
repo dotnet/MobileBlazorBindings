4e7c6e01f05bc60bf570588d4f8f6556569daf27
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2020-12-15 00:38:58 +0200

    Add EventHanders to Page and ScrollView

---
+++ src/Microsoft.MobileBlazorBindings/Elements/Handlers/PageHandler.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using Microsoft.MobileBlazorBindings.Core;
+
+namespace Microsoft.MobileBlazorBindings.Elements.Handlers
+{
+    public partial class PageHandler
+    {
+        partial void Initialize(NativeComponentRenderer renderer)
+        {
+            ConfigureEvent(
+                eventName: "onappearing",
+                setId: id => AppearingEventHandlerId = id,
+                clearId: id => { if (AppearingEventHandlerId == id) { AppearingEventHandlerId = 0; } });
+            PageControl.Appearing += (s, e) =>
+            {
+                if (AppearingEventHandlerId != default)
+                {
+                    renderer.Dispatcher.InvokeAsync(() => renderer.DispatchEventAsync(AppearingEventHandlerId, null, e));
+                }
+            };
+
+            ConfigureEvent(
+                eventName: "ondisappearing",
+                setId: id => DisappearingEventHandlerId = id,
+                clearId: id => { if (DisappearingEventHandlerId == id) { DisappearingEventHandlerId = 0; } });
+            PageControl.Disappearing += (s, e) =>
+            {
+                if (DisappearingEventHandlerId != default)
+                {
+                    renderer.Dispatcher.InvokeAsync(() => renderer.DispatchEventAsync(DisappearingEventHandlerId, null, e));
+                }
+            };
+        }
+
+        public ulong AppearingEventHandlerId { get; set; }
+        public ulong DisappearingEventHandlerId { get; set; }
+    }
+}
--- src/Microsoft.MobileBlazorBindings/Elements/ScrollView.cs
+++ src/Microsoft.MobileBlazorBindings/Elements/ScrollView.cs
@@ -2,15 +2,27 @@
 // Licensed under the MIT license.
 
 using Microsoft.AspNetCore.Components;
+using Microsoft.MobileBlazorBindings.Core;
+using Xamarin.Forms;
 
 namespace Microsoft.MobileBlazorBindings.Elements
 {
     public partial class ScrollView : Layout
     {
+        /// <summary>
+        /// Event that is raised after a scroll completes.
+        /// </summary>
+        [Parameter] public EventCallback<ScrolledEventArgs> OnScrolled { get; set; }
+
 #pragma warning disable CA1721 // Property names should not match get methods
         [Parameter] public RenderFragment ChildContent { get; set; }
 #pragma warning restore CA1721 // Property names should not match get methods
 
         protected override RenderFragment GetChildContent() => ChildContent;
+
+        partial void RenderAdditionalAttributes(AttributesBuilder builder)
+        {
+            builder.AddAttribute("onscrolled", OnScrolled);
+        }
     }
 }
