fbbc9b1237c11c0fc22ffd1c3930da85fdf6b841
Eilon Lipton <Eilon@users.noreply.github.com>
2021-01-12 15:07:18 -0800

    Update install-android-sdk.cmd

--- azure-pipelines/install-android-sdk.cmd
+++ azure-pipelines/install-android-sdk.cmd
@@ -1,2 +1,3 @@
 @echo off
+type yes.txt | "C:\Program Files (x86)\Android\android-sdk\tools\bin\sdkmanager.bat" --licenses
 type yes.txt | "C:\Program Files (x86)\Android\android-sdk\tools\bin\sdkmanager.bat" "platforms;android-29"
