eae5e7bb84a2a8efec76d4db8fb91f6f03dde66f
Eilon Lipton <Eilon@users.noreply.github.com>
2021-02-02 15:03:31 -0800

    Add WebView2 to WinForms control

--- src/Microsoft.MobileBlazorBindings.WindowsForms/BlazorWebView.cs
+++ src/Microsoft.MobileBlazorBindings.WindowsForms/BlazorWebView.cs
@@ -1,9 +1,22 @@
-﻿using System;
+﻿using Microsoft.Web.WebView2.WinForms;
+using System;
 using System.Windows.Forms;
 
 namespace Microsoft.MobileBlazorBindings.WindowsForms
 {
     public class BlazorWebView : Control
     {
+        private WebView2 _webView2;
+
+        public BlazorWebView()
+        {
+            _webView2 = new WebView2()
+            {
+                Dock = DockStyle.Fill,
+            };
+            Controls.Add(_webView2);
+
+            _webView2.Source = new Uri("https://www.microsoft.com");
+        }
     }
 }
