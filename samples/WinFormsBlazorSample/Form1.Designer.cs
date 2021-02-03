e9cab75888669574603929dcf96b8b502f1042f8
Eilon Lipton <Eilon@users.noreply.github.com>
2021-02-02 16:53:44 -0800

    WIP: WinForms BlazorWebView

--- samples/WinFormsBlazorSample/Form1.Designer.cs
+++ samples/WinFormsBlazorSample/Form1.Designer.cs
@@ -66,6 +66,7 @@ namespace WinFormsBlazorSample
             this.button1.TabIndex = 0;
             this.button1.Text = "&Click to see counter value";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // blazorWebView1
             // 
