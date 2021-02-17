bac3baf573b1d888048b06c965295b385f656be7
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2021-02-17 14:11:11 +0000

    Make a webview2 appear

--- src/Microsoft.MobileBlazorBindings.WPFNew/BlazorWebView.cs
+++ src/Microsoft.MobileBlazorBindings.WPFNew/BlazorWebView.cs
@@ -1,8 +1,32 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Web.WebView2.Wpf;
 
 namespace Microsoft.MobileBlazorBindings.WPFNew
 {
     public class BlazorWebView : Control
     {
+        private const string webViewTemplateChildName = "WebView";
+        private WebView2 _webview;
+
+        public BlazorWebView()
+        {
+            Template = new ControlTemplate
+            {
+                VisualTree = new FrameworkElementFactory(typeof(WebView2), webViewTemplateChildName)
+            };
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _webview = (WebView2)GetTemplateChild(webViewTemplateChildName);
+
+            Dispatcher.InvokeAsync(async () =>
+            {
+                await _webview.EnsureCoreWebView2Async().ConfigureAwait(true);
+                _webview.Source = new System.Uri("https://microsoft.com");
+            });
+        }
     }
 }
