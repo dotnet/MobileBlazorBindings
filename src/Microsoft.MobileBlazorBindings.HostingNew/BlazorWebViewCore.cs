e37a92cea050788e296dd6c13ce70cbc26f93322
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2021-02-18 14:38:15 +0000

    Begin factoring out platform-independent parts into a common layer

---
+++ src/Microsoft.MobileBlazorBindings.HostingNew/BlazorWebViewCore.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.StaticFiles;
+using System;
+using System.IO;
+using System.Text;
+
+namespace Microsoft.MobileBlazorBindings.HostingNew
+{
+    /// <summary>
+    /// Platform-agnostic parts of BlazorWebView
+    /// </summary>
+    public class BlazorWebViewCore
+    {
+        private static readonly FileExtensionContentTypeProvider FileExtensionContentTypeProvider = new();
+
+        public string ContentHost { get; private set; }
+        public string ContentRootPath { get; private set; }
+        public string HostPageRelativeUrl { get; private set; }
+
+        public void SetHostPage(string filePath)
+        {
+            var hostPageAbsolute = Path.GetFullPath(filePath);
+            ContentHost = "app";
+            ContentRootPath = Path.GetDirectoryName(hostPageAbsolute);
+            HostPageRelativeUrl = Path.GetRelativePath(ContentRootPath, hostPageAbsolute).Replace(Path.DirectorySeparatorChar, '/');
+        }
+
+        public bool TryGetResponseContent(Uri requestUri, out int statusCode, out string statusMessage, out Stream content, out string headers)
+        {
+            if (requestUri is null)
+            {
+                throw new ArgumentNullException(nameof(requestUri));
+            }
+
+            if (string.Equals(requestUri.Host, ContentHost, StringComparison.Ordinal))
+            {
+                var filePath = Path.GetFullPath(Path.Combine(ContentRootPath, requestUri.GetComponents(UriComponents.Path, UriFormat.Unescaped)));
+                if (filePath.StartsWith(ContentRootPath, StringComparison.Ordinal)
+                    && File.Exists(filePath))
+                {
+                    var responseContentType = FileExtensionContentTypeProvider.TryGetContentType(filePath, out var matchedContentType)
+                        ? matchedContentType
+                        : "application/octet-stream";
+
+                    statusCode = 200;
+                    statusMessage = "OK";
+                    headers = $"Content-Type: {responseContentType}{Environment.NewLine}Cache-Control: no-cache, max-age=0, must-revalidate, no-store";
+                    content = File.OpenRead(filePath);
+                }
+                else
+                {
+                    // Always provide a response to requests on the virtual domain, even if no file matches
+                    var message = $"There is no file at {filePath}";
+                    statusCode = 404;
+                    statusMessage = "Not found";
+                    headers = "Content-Type: text/plain";
+#pragma warning disable CA2000 // Dispose objects before losing scope
+                    content = new MemoryStream(Encoding.UTF8.GetBytes(message));
+#pragma warning restore CA2000 // Dispose objects before losing scope
+                }
+
+                return true;
+            }
+
+            statusCode = default;
+            statusMessage = default;
+            headers = default;
+            content = default;
+            return false;
+        }
+    }
+}
--- src/Microsoft.MobileBlazorBindings.WPFNew/BlazorWebView.cs
+++ src/Microsoft.MobileBlazorBindings.WPFNew/BlazorWebView.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.MobileBlazorBindings.HostingNew;
 using Microsoft.Web.WebView2.Core;
 using Microsoft.Web.WebView2.Wpf;
 
@@ -11,7 +10,7 @@ namespace Microsoft.MobileBlazorBindings.WPFNew
     public class BlazorWebView : Control
     {
         private const string webViewTemplateChildName = "WebView";
-        private static readonly FileExtensionContentTypeProvider FileExtensionContentTypeProvider = new();
+        private readonly BlazorWebViewCore _core = new();
         private WebView2 _webview;
         private CoreWebView2Environment _webviewEnvironment;
 
@@ -25,17 +24,12 @@ namespace Microsoft.MobileBlazorBindings.WPFNew
 
         public string HostPage { get; set; }
 
-        private string _contentRoot;
-        private string _hostPageRelativeUrl;
-
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
-            _webview = (WebView2)GetTemplateChild(webViewTemplateChildName);
 
-            var hostPageAbsolute = Path.GetFullPath(HostPage);
-            _contentRoot = Path.GetDirectoryName(hostPageAbsolute);
-            _hostPageRelativeUrl = Path.GetRelativePath(_contentRoot, hostPageAbsolute).Replace(Path.DirectorySeparatorChar, '/');
+            _core.SetHostPage(HostPage);
+            _webview = (WebView2)GetTemplateChild(webViewTemplateChildName);
 
             Dispatcher.InvokeAsync(async () =>
             {
@@ -46,37 +40,27 @@ namespace Microsoft.MobileBlazorBindings.WPFNew
 
                     _webview.CoreWebView2.AddWebResourceRequestedFilter("*", CoreWebView2WebResourceContext.All);
                     _webview.CoreWebView2.WebResourceRequested += HandleWebResourceRequested;
-                    _webview.Source = new Uri(new Uri("https://app/"), _hostPageRelativeUrl);
+                    _webview.Source = new Uri(new Uri($"https://{_core.ContentHost}/"), _core.HostPageRelativeUrl);
                 }
                 catch (Exception ex)
                 {
                     // TODO: Figure out how to ensure any exceptions propagate to a central handler
                     // without needing this try/catch
                     MessageBox.Show(ex.ToString());
+                    throw;
                 }
             });
         }
 
         private void HandleWebResourceRequested(object sender, CoreWebView2WebResourceRequestedEventArgs args)
         {
-            var request = args.Request;
-            var requestUri = new Uri(request.Uri);
-            if (string.Equals(requestUri.Host, "app", StringComparison.Ordinal))
+            if (_core.TryGetResponseContent(new Uri(args.Request.Uri), out var statusCode, out var statusMessage, out var content, out var headers))
             {
-                var filePath = Path.GetFullPath(Path.Combine(_contentRoot, requestUri.GetComponents(UriComponents.Path, UriFormat.Unescaped)));
-                if (filePath.StartsWith(_contentRoot, StringComparison.Ordinal)
-                    && File.Exists(filePath))
-                {
-                    var responseContentType = FileExtensionContentTypeProvider.TryGetContentType(filePath, out var matchedContentType)
-                        ? matchedContentType
-                        : "application/octet-stream";
-
-                    args.Response = _webviewEnvironment.CreateWebResourceResponse(
-                        Content: File.OpenRead(filePath),
-                        StatusCode: 200,
-                        ReasonPhrase: "OK",
-                        Headers: $"Content-Type: {responseContentType}{Environment.NewLine}Cache-Control: no-cache, max-age=0, must-revalidate, no-store");
-                }
+                args.Response = _webviewEnvironment.CreateWebResourceResponse(
+                        content,
+                        statusCode,
+                        statusMessage,
+                        headers);
             }
         }
     }
