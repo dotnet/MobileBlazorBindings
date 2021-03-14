da2b8230d3f6ab7fbbcc941a25bb50eeb7f58624
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2021-03-15 01:44:36 +0200

    Add support for DataTemplate properties (from RenderFragment<T>)

--- src/Microsoft.MobileBlazorBindings/RenderTreeBuilderHelper.cs
+++ src/Microsoft.MobileBlazorBindings/RenderTreeBuilderHelper.cs
@@ -4,6 +4,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
 using Microsoft.MobileBlazorBindings.Elements;
+using Microsoft.MobileBlazorBindings.Elements.DataTemplates;
 using System;
 
 namespace Microsoft.MobileBlazorBindings
@@ -21,13 +22,13 @@ namespace Microsoft.MobileBlazorBindings
             {
                 builder.OpenRegion(sequence);
 
-                // Content properties are handled by separate handlers, there rendered as separate child elements.
+                // Content properties are handled by separate handlers, therefore rendered as separate child elements.
                 // Renderer does not support elements without parent components as for now,
                 // therefore adding empty parent component to workaround that.
                 builder.OpenComponent<PropertyWrapperComponent>(0);
                 builder.AddAttribute(1, "ChildContent", (RenderFragment)(builder =>
                 {
-                    builder.OpenElement(2, $"p-{containingType.FullName}.{propertyName}");
+                    builder.OpenElement(2, GetElementName(containingType, propertyName));
                     builder.AddContent(3, content);
                     builder.CloseElement();
                 }));
@@ -35,6 +36,32 @@ namespace Microsoft.MobileBlazorBindings
 
                 builder.CloseRegion();
             }
+        }
+
+        public static void AddDataTemplateProperty<T>(RenderTreeBuilder builder, int sequence, Type containingType, string propertyName, RenderFragment<T> template)
+        {
+            if (builder is null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+            if (containingType is null)
+            {
+                throw new ArgumentNullException(nameof(containingType));
+            }
+
+            builder.OpenRegion(sequence);
+
+            builder.OpenComponent<DataTemplateItemsComponent<T>>(0);
+            builder.AddAttribute(1, nameof(DataTemplateItemsComponent<T>.ElementName), GetElementName(containingType, propertyName));
+            builder.AddAttribute(2, nameof(DataTemplateItemsComponent<T>.Template), template);
+            builder.CloseComponent();
+
+            builder.CloseRegion();
+        }
+
+        private static string GetElementName(Type containingType, string propertyName)
+        {
+            return $"p-{containingType.FullName}.{propertyName}";
         }
     }
 }
