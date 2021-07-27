e448cd99f6ad88221fd325708c4daf30334efc59
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2021-07-27 14:05:26 +0300

    Add Background property to VisualElement

--- src/Microsoft.MobileBlazorBindings/Elements/VisualElement.cs
+++ src/Microsoft.MobileBlazorBindings/Elements/VisualElement.cs
@@ -2,13 +2,22 @@
 // Licensed under the MIT license.
 
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
 using Microsoft.MobileBlazorBindings.Core;
+using Microsoft.MobileBlazorBindings.Elements.Handlers;
 using XF = Xamarin.Forms;
 
 namespace Microsoft.MobileBlazorBindings.Elements
 {
     public partial class VisualElement : NavigableElement
     {
+        static partial void RegisterAdditionalHandlers()
+        {
+            ElementHandlerRegistry.RegisterPropertyContentHandler<VisualElement>(nameof(Background),
+                _ => new ContentPropertyHandler<XF.VisualElement>((visualElement, contentElement) => visualElement.Background = (XF.Brush)contentElement));
+        }
+
+        [Parameter] public RenderFragment Background { get; set; }
         [Parameter] public EventCallback<XF.FocusEventArgs> OnFocused { get; set; }
         [Parameter] public EventCallback OnSizeChanged { get; set; }
         [Parameter] public EventCallback<XF.FocusEventArgs> OnUnfocused { get; set; }
@@ -18,6 +27,11 @@ namespace Microsoft.MobileBlazorBindings.Elements
             builder.AddAttribute("onfocused", OnFocused);
             builder.AddAttribute("onsizechanged", OnSizeChanged);
             builder.AddAttribute("onunfocused", OnUnfocused);
+        }
+
+        protected override void RenderAdditionalElementContent(RenderTreeBuilder builder, ref int sequence)
+        {
+            RenderTreeBuilderHelper.AddContentProperty(builder, sequence++, typeof(VisualElement), nameof(Background), Background);
         }
     }
 }
