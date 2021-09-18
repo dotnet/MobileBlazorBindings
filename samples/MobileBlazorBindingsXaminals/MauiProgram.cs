8c5eac3fbd23dc0c9a912af7d38f035c829a2221
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2021-09-18 21:33:27 +0300

    Update Xaminals sample for MAUI and updated MBB

---
+++ samples/MobileBlazorBindingsXaminals/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+using Microsoft.MobileBlazorBindings;
+
+namespace MobileBlazorBindingsXaminals
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services
+                .AddSingleton<ShellNavigationManager>()
+                .AddScoped<MobileBlazorBindingsRenderer>();
+
+            return builder.Build();
+        }
+    }
+}
---
+++ samples/MobileBlazorBindingsXaminals/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,8 @@
+﻿namespace MobileBlazorBindingsXaminals
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
