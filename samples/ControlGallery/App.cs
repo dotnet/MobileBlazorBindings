a6e74fc77b66acd7dc81e3856e3a1f6f2d20133a
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2021-09-25 23:41:10 +0300

    Migrate ControlGallery sample (no SkiaSharp)

---
+++ samples/ControlGallery/App.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using Microsoft.Maui.Controls;
+using Microsoft.MobileBlazorBindings;
+
+namespace ControlGallery
+{
+    public class App : Application
+    {
+        public App(MobileBlazorBindingsRenderer renderer)
+        {
+            MainPage = new ContentPage();
+            _ = renderer.AddComponent<AppShell>(parent: MobileBlazorBindingsHostExtensions.CreateHandler(this, renderer));
+        }
+    }
+}
