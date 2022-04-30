8ffe632cbec8a53f68d1695b31aafa5dd9bdff36
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2022-04-30 21:36:06 +0300

    samples fixes

--- samples/HybridAuthSample/Platforms/Android/MainActivity.cs
+++ samples/HybridAuthSample/Platforms/Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace HybridAuthSample
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, Exported = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : MauiAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- src/Microsoft.MobileBlazorBindings.Authentication/Platforms/Android/WebAuthenticationCallbackActivity.cs
+++ src/Microsoft.MobileBlazorBindings.Authentication/Platforms/Android/WebAuthenticationCallbackActivity.cs
@@ -10,7 +10,7 @@ namespace Microsoft.MobileBlazorBindings.Authentication
     /// <summary>
     /// Handles callback for authentication on Android.
     /// </summary>
-    [Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop)]
+    [Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop, Exported = true)]
     [IntentFilter(new[] { Intent.ActionView }, Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable }, DataScheme = "app")]
     public class WebAuthenticationCallbackActivity : Maui.Authentication.WebAuthenticatorCallbackActivity
     {
