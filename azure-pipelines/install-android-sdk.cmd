f13fc80ae63dda8df9f25c792292a849248b2986
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2022-05-06 21:35:12 +0300

    Remove various Microsoft CI stuff

--- azure-pipelines/install-android-sdk.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-type "%~dp0\yes.txt" | "C:\Program Files (x86)\Android\android-sdk\tools\bin\sdkmanager.bat" --licenses
-type "%~dp0\yes.txt" | "C:\Program Files (x86)\Android\android-sdk\tools\bin\sdkmanager.bat" "platforms;android-29"
--- init.cmd
+++
@@ -1,3 +0,0 @@
-@set PS1UnderCmd=1
-powershell.exe -ExecutionPolicy bypass -Command "& '%~dpn0.ps1'" %*
-@set PS1UnderCmd=
