70af60d37e7318a41af40028ef36de6cfd1c626a
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2022-05-06 21:13:52 +0300

    Remove Microsoft.MobileBlazorBindings.WebView projects

--- src/Microsoft.MobileBlazorBindings.WebView.Android/Exports.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-using Microsoft.MobileBlazorBindings.WebView.Android;
-using Microsoft.MobileBlazorBindings.WebView.Elements;
-using Xamarin.Forms;
-
-[assembly: ExportRenderer(typeof(WebViewExtended), typeof(WebKitWebViewRenderer))]
--- src/Microsoft.MobileBlazorBindings.WebView.JS/upstream/aspnetcore/web.js/src/BootCommon.ts
+++
@@ -1,6 +0,0 @@
-// Tells you if the script was added without <script src="..." autostart="false"></script>
-export function shouldAutoStart() {
-  return !!(document &&
-    document.currentScript &&
-    document.currentScript.getAttribute('autostart') !== 'false');
-}
