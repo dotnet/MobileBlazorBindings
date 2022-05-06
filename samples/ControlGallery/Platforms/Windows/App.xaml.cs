c37a91beca6138d7a87a3f61c25848712955eb69
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2022-05-07 02:17:37 +0300

    Samples fixes

--- samples/ControlGallery/Platforms/Windows/App.xaml.cs
+++ samples/ControlGallery/Platforms/Windows/App.xaml.cs
@@ -23,12 +23,5 @@ namespace ControlGallery.WinUI
         }
 
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-
-        protected override void OnLaunched(LaunchActivatedEventArgs args)
-        {
-            base.OnLaunched(args);
-
-            Microsoft.Maui.Essentials.Platform.OnLaunched(args);
-        }
     }
 }
--- samples/HybridAuthSample/Platforms/iOS/AppDelegate.cs
+++ samples/HybridAuthSample/Platforms/iOS/AppDelegate.cs
@@ -1,16 +1,11 @@
-﻿namespace HybridAuthSample
+﻿using Android.Runtime;
+using Foundation;
+
+namespace MauiApp1
 {
-	[Register("AppDelegate")]
-	public class AppDelegate : MauiUIApplicationDelegate
-	{
-		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-
-		public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
-		{
-			if (Platform.OpenUrl(app, url, options))
-				return true;
-
-			return base.OpenUrl(app, url, options);
-		}
-	}
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
 }
