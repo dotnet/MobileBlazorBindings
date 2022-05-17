ef7ae524a734086477d749e9cc1521e5f8b28fdf
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2022-05-17 23:48:31 +0300

    Add some AttributeHelperTests

---
+++ src/BlazorBindings.UnitTests/Elements/AttributeHelperTests.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using BlazorBindings.Maui.Elements;
+using NUnit.Framework;
+
+namespace BlazorBindings.UnitTests.Elements
+{
+    public class AttributeHelperTests
+    {
+        [Test]
+        public void AttributeValueShouldReturnSameObjectInstance()
+        {
+            var originalInstance = new object();
+            var attributeValue = AttributeHelper.ObjectToDelegate(originalInstance);
+            var retrievedInstance = AttributeHelper.DelegateToObject<object>(attributeValue);
+
+            Assert.That(retrievedInstance, Is.SameAs(originalInstance));
+        }
+
+        [Test]
+        public void AttributeValuesShouldBeEqualForTheSameObject()
+        {
+            var instance = new object();
+
+            var attributeValue1 = AttributeHelper.ObjectToDelegate(instance);
+            var attributeValue2 = AttributeHelper.ObjectToDelegate(instance);
+
+            Assert.That(attributeValue1.Equals(attributeValue2));
+        }
+
+        [Test]
+        public void AttributeValuesShouldNotBeEqualForDifferentObject()
+        {
+            var instance1 = new object();
+            var instance2 = new object();
+
+            var attributeValue1 = AttributeHelper.ObjectToDelegate(instance1);
+            var attributeValue2 = AttributeHelper.ObjectToDelegate(instance2);
+
+            Assert.That(!attributeValue1.Equals(attributeValue2));
+        }
+    }
+}
